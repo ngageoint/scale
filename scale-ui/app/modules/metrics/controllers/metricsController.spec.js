202663c76c5ea2f7922e5ea5ed48b1a3920294cd
ericsvendsen <esvendsen@gmail.com>
2016-01-27 09:56:16 -0500

    adding base framework for frontend tests

---
+++ scale-ui/app/modules/metrics/controllers/metricsController.spec.js
@@ -0,0 +1,8 @@
+(function () {
+    describe('just a test', function () {
+        it('should equal true', function () {
+            console.log('testing...');
+            expect(1).toEqual(1);
+        });
+    });
+})();
