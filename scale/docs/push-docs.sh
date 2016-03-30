5326ad63b02093aa17f161cc382a90bbf1c8756e
Jonathan Meyer <jonathan.meyer@appliedis.com>
2016-03-30 13:39:53 -0400

    Updated push-docs to include checkout

--- scale/docs/push-docs.sh
+++ scale/docs/push-docs.sh
@@ -5,9 +5,10 @@ echo "Host github.com" >> ~/.ssh/config
 echo "  IdentityFile ~/.ssh/publish-key" >> ~/.ssh/config
 git remote set-url origin git@github.com:ngageoint/scale.git
 git fetch origin -f gh-pages:gh-pages
+git checkout gh-pages
 
-cp -R _build/html/ ../../docs
 git rm -fr ../../docs
+cp -R _build/html/ ../../docs
 git add ../../docs
 
 git config --global user.email "no-reply@travis-ci.org"
