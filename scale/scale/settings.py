418ee07368051149df9a1a7c2978d278903d8106
Chris Ward <cward@ball.com>
2016-04-03 00:19:32 -0400

    Upgraded to DRF 3.2.

--- scale/scale/settings.py
+++ scale/scale/settings.py
@@ -98,6 +98,7 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.AdminRenderer',
     ),
 }
 
