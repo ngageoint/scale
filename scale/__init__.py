f67473cfa31b2555bf2ea4cdeea22f37fdbba04b
Trevor R.H. Clarke <tclarke@ball.com>
2016-04-07 11:50:23 -0400

    Add version info to the top level module

---
+++ scale/__init__.py
@@ -0,0 +1,10 @@
+"""Django project root used for settings and deployment configuration."""
+
+from __future__ import absolute_import
+
+VERSION = (3, 0, 0)
+__version__ = '.'.join(str(p) for p in VERSION[0:3]) + ''.join(VERSION[3:])
+__author__ = 'NGA'
+__contact__ = ''
+__homepage__ = 'https://ngageoint.github.io/scale'
+__docformat__ = 'restructuredtext'
