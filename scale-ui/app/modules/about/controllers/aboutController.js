446e275e428fffeb6356888e250fa71f014f5cff
ericsvendsen-mil <eric.svendsen.ctr@us.af.mil>
2016-04-22 10:34:04 -0400

    added logic to obtain current version and add it to the about page for issue #29

--- scale-ui/app/modules/about/controllers/aboutController.js
+++ scale-ui/app/modules/about/controllers/aboutController.js
@@ -1,10 +1,16 @@
 (function () {
     'use strict';
 
-    angular.module('scaleApp').controller('aboutController', function($scope, $location, $window, navService) {
+    angular.module('scaleApp').controller('aboutController', function($scope, $location, $window, navService, scaleService) {
+        $scope.version = '';
+
         var initialize = function() {
             navService.updateLocation('about');
+            scaleService.getVersion().then(function (data) {
+                $scope.version = data.version;
+            });
         };
+
         initialize();
     });
 })();
