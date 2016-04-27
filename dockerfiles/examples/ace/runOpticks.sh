ef5250d7a57a4983fb35f6ea52f93a5f2d46199a
Trevor R.H. Clarke <tclarke@ball.com>
2016-04-27 12:21:26 -0400

    Revert "Go cli (#198)" Acceidental merge to master instead of develop.
    This reverts commit e6b7f07fb220c3256cb0fe411fc42a17eb6a8004.

--- dockerfiles/examples/ace/runOpticks.sh
+++ dockerfiles/examples/ace/runOpticks.sh
@@ -1,11 +1,6 @@
 #!/bin/bash -x
 
-export wavelength_file=$3
-if [[ wavelength_file == "" ]]; then
-    export wavelength_file=/wizards/oli-vis.wmd
-fi
-export outfile=$4/$(basename -s .tif $1)_results.tif
-export outshpfile=$4/$(basename -s .tif $1)_results.shp
+export outfile=$2/$(basename -s .tif $1)_results.tif
 cp /wizards/ace.wiz /tmp/
 cat << EOF | python
 import xml.etree.ElementTree as ET
@@ -14,26 +9,21 @@ tree = ET.parse('/wizards/ace.batchwiz')
 ns={"opticks":"https://comet.balldayton.com/standards/namespaces/2005/v1/comet.xsd"}
 
 tree.find('.//opticks:parameter[@name="Input Filename"]/opticks:value', ns).text = "file://$1"
-tree.find('.//opticks:parameter[@name="Wavelength File"]/opticks:value', ns).text = "file://${wavelength_file}"
-tree.find('.//opticks:parameter[@name="Signature Filename"]/opticks:value', ns).text = "file://$2"
+tree.find('.//opticks:parameter[@name="Wavelength File"]/opticks:value', ns).text = "file:///wizards/oli-vis.wmd"
+tree.find('.//opticks:parameter[@name="Signature Filename"]/opticks:value', ns).text = "file:///wizards/jhu.becknic.manmade.asphalt.paving.solid.0095uuu.spectrum.txt"
 tree.find('.//opticks:parameter[@name="Output Filename"]/opticks:value', ns).text = "file://${outfile}"
-tree.find('.//opticks:parameter[@name="Shapefile Filename"]/opticks:value', ns).text = "file://${outshpfile}"
 tree.write('/tmp/ace.batchwiz')
 EOF
 
 /opt/Opticks/Bin/OpticksBatch -input:/tmp/ace.batchwiz
 
 # write results manifest
-cat > $4/results_manifest.json << EOF
+cat > $2/results_manifest.json << EOF
 { "version": "1.1",
   "output_data": [{
     "name": "results",
     "file": {
         "path": "${outfile}"
-    },
-    "name": "centroids",
-    "file": {
-        "path": "${outshpfile}"
     }
   }]
 }
