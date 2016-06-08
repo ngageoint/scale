fad03352156b81adc3c178df742f73b8c6ad3fab
Derick Faller <derick.faller.ctr@us.af.mil>
2016-06-08 14:48:16 -0400

    Moved recipe data migration to its own migration file

--- scale/recipe/migrations/0012_auto_20160330_1659.py
+++ scale/recipe/migrations/0012_auto_20160330_1659.py
@@ -12,28 +12,6 @@ class Migration(migrations.Migration):
         ('recipe', '0011_auto_20160330_1505'),
     ]
 
-    def populate_recipe_job(apps, schema_editor):
-        # Go through all of the old recipe_job models and create new ones for the new schema
-        RecipeJob = apps.get_model('recipe', 'RecipeJob')
-        RecipeJobOld = apps.get_model('recipe', 'RecipeJobOld')
-        recipe_jobs_to_create = []
-        for recipe_job_old in RecipeJobOld.objects.all().iterator():
-            recipe_job = RecipeJob()
-            recipe_job.recipe_id = recipe_job_old.recipe_id
-            recipe_job.job_id = recipe_job_old.job_id
-            recipe_job.job_name = recipe_job_old.job_name
-            recipe_jobs_to_create.append(recipe_job)
-            if len(recipe_jobs_to_create) >= 500:
-                RecipeJob.objects.bulk_create(recipe_jobs_to_create)
-                recipe_jobs_to_create = []
-        if recipe_jobs_to_create:
-            RecipeJob.objects.bulk_create(recipe_jobs_to_create)
-        old_count = RecipeJobOld.objects.all().count()
-        new_count = RecipeJob.objects.all().count()
-        if old_count != new_count:
-            raise Exception('Failed conversion: There are %s old recipe job models and %s new recipe models',
-                            str(old_count), str(new_count))
-
     operations = [
         migrations.AlterField(
             model_name='recipejobold',
@@ -55,5 +33,4 @@ class Migration(migrations.Migration):
             },
             bases=(models.Model,),
         ),
-        migrations.RunPython(populate_recipe_job),
     ]
---
+++ scale/recipe/migrations/0014_auto_20160608_1402.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('recipe', '0013_auto_20160331_1127'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='recipejobold',
+            name='job',
+        ),
+        migrations.RemoveField(
+            model_name='recipejobold',
+            name='recipe',
+        ),
+        migrations.DeleteModel(
+            name='recipejobold',
+        ),
+    ]
