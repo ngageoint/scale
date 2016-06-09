58482f49b55f4d5791f2c21418df46786c240a2f
Derick Faller <derick.faller.ctr@us.af.mil>
2016-06-09 11:39:14 -0400

    Corrected recipe data migration so it doesn't run out of memory

--- scale/recipe/migrations/0013_auto_20160331_1127.py
+++ scale/recipe/migrations/0013_auto_20160331_1127.py
@@ -15,17 +15,24 @@ class Migration(migrations.Migration):
         RecipeJob = apps.get_model('recipe', 'RecipeJob')
         RecipeJobOld = apps.get_model('recipe', 'RecipeJobOld')
         recipe_jobs_to_create = []
-        for recipe_job_old in RecipeJobOld.objects.all().iterator():
-            recipe_job = RecipeJob()
-            recipe_job.recipe_id = recipe_job_old.recipe_id
-            recipe_job.job_id = recipe_job_old.job_id
-            recipe_job.job_name = recipe_job_old.job_name
-            recipe_jobs_to_create.append(recipe_job)
-            if len(recipe_jobs_to_create) >= 500:
-                RecipeJob.objects.bulk_create(recipe_jobs_to_create)
-                recipe_jobs_to_create = []
-        if recipe_jobs_to_create:
+        total_count = RecipeJobOld.objects.all().count()
+        print 'Populating new recipe_job table: %s rows' % str(total_count)
+        done_count = 0
+        batch_size = 500
+        while done_count < total_count:
+            percent = (float(done_count) / float(total_count)) * 100.00
+            print 'Completed %s of %s recipe jobs (%f%%)' % (done_count, total_count, percent)
+            batch_end = done_count + batch_size
+            for recipe_job_old in RecipeJobOld.objects.all().order_by('job_id')[done_count:batch_end]:
+                recipe_job = RecipeJob()
+                recipe_job.recipe_id = recipe_job_old.recipe_id
+                recipe_job.job_id = recipe_job_old.job_id
+                recipe_job.job_name = recipe_job_old.job_name
+                recipe_jobs_to_create.append(recipe_job)
             RecipeJob.objects.bulk_create(recipe_jobs_to_create)
+            recipe_jobs_to_create = []
+            done_count += batch_size
+        print 'All %s recipe jobs completed' % str(total_count)
         old_count = RecipeJobOld.objects.all().count()
         new_count = RecipeJob.objects.all().count()
         if old_count != new_count:
