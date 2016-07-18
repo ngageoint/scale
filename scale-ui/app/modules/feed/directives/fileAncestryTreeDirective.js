dbed748b5b24deec225a5d4277e52b1221c8e923
Chris Dickman <chris.dickman@appliedis.com>
2016-07-18 13:38:26 -0400

    added file tree visualization showing ingested sources files as input to jobs and output files from those jobs

--- scale-ui/app/modules/feed/directives/fileAncestryTreeDirective.js
+++ scale-ui/app/modules/feed/directives/fileAncestryTreeDirective.js
@@ -2,171 +2,140 @@
  * <scale-file-ancestry-tree />
  */
 (function () {
-    angular.module('scaleApp').controller('scaleFileAncestryTreeController', function ($scope) {
-        $scope.source = null;
+    angular.module('scaleApp').controller('scaleFileAncestryTreeController', function ($scope, scaleService) {
+        var vm = this;
+        vm.source = $scope.source;
 
+        $scope.$watchCollection('vm.source', function() {
+            draw();
+            //console.log(vm.source);
+        });
 
-        var treeData = [
-            {
-                "name": "Top Level",
-                "parent": "null",
-                "children": [
-                    {
-                        "name": "Level 2: A",
-                        "parent": "Top Level"
-                        //"children": [
-                        //    {
-                        //        "name": "Son of A",
-                        //        "parent": "Level 2: A"
-                        //    },
-                        //    {
-                        //        "name": "Daughter of A",
-                        //        "parent": "Level 2: A"
-                        //    }
-                        //]
-                    },
-                    {
-                        "name": "Level 2: B",
-                        "parent": "Top Level"
-                    }
-                ]
-            }
-        ];
+        function getNodeHtml(file) {
+            var iconHtml = scaleService.getMediaTypeHtml(file.media_type);
+            var html = "<div class='graph'>";
+            html += "<span class='name'><a ng-show='" + file.url + "' href='" + file.url + "' target='_jobfile'>" + iconHtml + ' ' + file.file_name + "</a></span>";
+            html += "</div>";
 
+            return html;
+        };
 
-// ************** Generate the tree diagram	 *****************
-        var margin = {top: 20, right: 120, bottom: 20, left: 120};
-        var width = 500 - margin.right - margin.left;
-        var height = 400 - margin.top - margin.bottom;
+        function getJobNodeHtml(jobtype, jobid){
+            var html = "<div>";
+            html += "<span class='status'></span><span class='job-name'>" + jobtype.name + ":" + jobid + "</span>";
+            html += "</div>";
 
-        var i = 0,
-            duration = 750,
-            root;
+            return html;
+        }
 
-        var tree = d3.layout.tree()
-            .size([height, width]);
+        function draw() {
+            if(vm.source && vm.source !== undefined){
+                var svg = d3.select("svg");
+                var inner = svg.select("g");
 
-        var diagonal = d3.svg.diagonal()
-            .projection(function(d) { return [d.y, d.x]; });
+                // Set up zoom support
+                var zoom = d3.behavior.zoom()
+                    .on("zoom", function() {
+                        // don't zoom on WheelEvent or MouseEvent, just window sizing
+                        if(d3.event.sourceEvent === null){
+                            inner.attr("transform", "translate(" + d3.event.translate + ")" +
+                                "scale(" + d3.event.scale + ")");
+                        }
+                });
+                svg.call(zoom);
 
-        var svg = d3.select("svg");
-            //.attr("width", width + margin.right + margin.left)
-            //.attr("height", height + margin.top + margin.bottom)
-        var inner = svg.append("g");
-
-        inner.attr("transform", "translate(" + margin.left/1.5 + "," + margin.top/2 + ")," +
-                "scale(" + 1 + ")");
-        inner.attr("class", "tree-group");
-
-
-        root = treeData[0];
-        root.x0 = height / 2;
-        root.y0 = 0;
-
-        update(root);
-
-        d3.select(self.frameElement).style("height", "500px");
-
-        function update(source) {
-
-            // Compute the new tree layout.
-            var nodes = tree.nodes(root).reverse(),
-                links = tree.links(nodes);
-
-            // Normalize for fixed-depth.
-            nodes.forEach(function(d) { d.y = d.depth * 180; });
-
-            // Update the nodes…
-            var node = inner.selectAll("g.node")
-                .data(nodes, function(d) { return d.id || (d.id = ++i); });
-
-            // Enter any new nodes at the parent's previous position.
-            var nodeEnter = node.enter().append("g")
-                .attr("class", "node")
-                .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; });
-                //.on("click", click);
-
-            nodeEnter.append("circle")
-                .attr("r", 1e-6)
-                .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
-
-            nodeEnter.append("text")
-                .attr("x", function(d) { return d.children || d._children ? -13 : 13; })
-                .attr("dy", ".35em")
-                .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
-                .text(function(d) { return d.name; })
-                .style("fill-opacity", 1e-6);
-
-            // Transition nodes to their new position.
-            var nodeUpdate = node.transition()
-                .duration(duration)
-                .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
-
-            nodeUpdate.select("circle")
-                .attr("r", 10)
-                .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
-
-            nodeUpdate.select("text")
-                .style("fill-opacity", 1);
-
-            // Transition exiting nodes to the parent's new position.
-            var nodeExit = node.exit().transition()
-                .duration(duration)
-                .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
-                .remove();
-
-            nodeExit.select("circle")
-                .attr("r", 1e-6);
-
-            nodeExit.select("text")
-                .style("fill-opacity", 1e-6);
-
-            // Update the links…
-            var link = inner.selectAll("path.link")
-                .data(links, function(d) { return d.target.id; });
-
-            // Enter any new links at the parent's previous position.
-            link.enter().insert("path", "g")
-                .attr("class", "link")
-                .attr("d", function(d) {
-                    var o = {x: source.x0, y: source.y0};
-                    return diagonal({source: o, target: o});
+                var render = new dagreD3.render();
+                // Left-to-right layout
+                var g = new dagreD3.graphlib.Graph({multigraph: true});
+                g.setGraph({
+                    nodesep: 30,
+                    ranksep: 70,
+                    rankdir: "LR",
+                    marginx: 20,
+                    marginy: 0
                 });
 
-            // Transition links to their new position.
-            link.transition()
-                .duration(duration)
-                .attr("d", diagonal);
+                //Starting node
+                var html = getNodeHtml(vm.source);
+                g.setNode('file-' + vm.source.id, {
+                    labelType: "html",
+                    label: html,
+                    rx: 10,
+                    ry: 10,
+                    padding: 10
+                    //width: 150,
+                    //height: 50
+                });
 
-            // Transition exiting nodes to the parent's new position.
-            link.exit().transition()
-                .duration(duration)
-                .attr("d", function(d) {
-                    var o = {x: source.x, y: source.y};
-                    return diagonal({source: o, target: o});
-                })
-                .remove();
+                // Products
+                for (var id in vm.source.products) {
+                    var prod = vm.source.products[id];
+                    var jobNodeHtml = getJobNodeHtml(prod.job_type, prod.job.id);
+                    // Add node for the job
+                    g.setNode('job-' + prod.job.id, {
+                        labelType: "html",
+                        label: jobNodeHtml,
+                        rx: 10,
+                        ry: 10,
+                        padding: 10
+                        //width: 150,
+                        //height: 50
+                    });
+                    g.setEdge('file-'+vm.source.id, 'job-'+prod.job.id, {
+                        //labelType: "html",
+                        //label: "<a href='#/jobs/job/" + prod.job.id + "'>" + prod.job_type.name + "</a>"
+                        //width: 40
+                    });
 
-            // Stash the old positions for transition.
-            nodes.forEach(function(d) {
-                d.x0 = d.x;
-                d.y0 = d.y;
-            });
 
-        }
+                    var html = getNodeHtml(prod);
+                    g.setNode('file-' + prod.id, {
+                        labelType: "html",
+                        label: html,
+                        rx: 10,
+                        ry: 10,
+                        padding: 10
+                        //width: 150,
+                        //height: 50
+                    });
+                    g.setEdge('job-'+prod.job.id, 'file-'+prod.id, {
+                        //labelType: "html",
+                        //label: "<a href='#/jobs/job/" + prod.job.id + "'>" + prod.job_type.name + "</a>"
+                        //width: 40
+                    });
 
-// Toggle children on click.
-        function click(d) {
-            if (d.children) {
-                d._children = d.children;
-                d.children = null;
-            } else {
-                d.children = d._children;
-                d._children = null;
+                }
+
+                var render = new dagreD3.render();
+
+                // Run the renderer. This is what draws the final graph.
+                render(inner, g);
+
+                // Center the graph
+                var vbx = svg.attr('viewBox').split(' ');
+                var gwidth = g.graph().width;
+                var gheight = g.graph().height;
+                var ratio = gheight/gwidth;
+                var vbxheight = vbx[2]*ratio;
+                svg.attr('viewBox', vbx[0] + ' ' + vbx[1] + ' ' + vbx[2] + ' ' + vbxheight);
+
+                var initialScale = vbx[2]/gwidth;
+
+                zoom
+                    .translate([0, 0])
+                    .scale(initialScale)
+                    .event(svg);
+
+
+                // set appropriate class for job nodes
+                $('.job-name').closest('.node').children('rect').attr('class', 'job'); //.addClass('job');
+
             }
-            update(d);
+
         }
-        
+
+
+
     }).directive('scaleFileAncestryTree', function () {
         'use strict';
         /**
