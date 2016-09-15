7dc9be4ba672fe2107b7b11aafe6a25192b1162c
Jonathan Meyer <jon@gisjedi.com>
2016-09-15 10:32:50 -0400

    Updates to deployment script to use new logstash image

--- dockerfiles/framework/scale/deployElk.py
+++ dockerfiles/framework/scale/deployElk.py
@@ -7,51 +7,60 @@ import requests, os, json, time
 
 
 def run():
-    # get the elasticsearch API endpoints
-    es_url = os.environ.get('SCALE_ELASTICSEARCH_URL', 'http://elasticsearch.marathon.mesos:31105')
-    config_uri = os.environ.get('CONFIG_URI', '')
-    if config_uri:
-        config_uri = '"' + config_uri + '"'  # Config URI needs quotes to be inserted into JSON string
-    endpoints = [x['http_address'] for x in json.loads(requests.get(es_url+'/v1/tasks').text)]
-
-    marathon = '''
-    {
-      "id": "scale-logstash",
-      "cmd": "logstash --allow-env --verbose -e 'input { gelf { port => \\"${PORT0}\\" } } output { elasticsearch { hosts => [\\"${ES_HOST}\\"] } }'",
-      "cpus": 0.5,
-      "mem": 1024,
-      "disk": 256,
-      "instances": 1,
-      "container": {
-        "docker": {
-          "image": "%s",
-          "network": "HOST",
-          "forcePullImage": true
+    # get the Logstash container API endpoints
+    logstash_image = os.getenv('LOGSTASH_DOCKER_IMAGE', 'appliedis/dcos-logstash:2.4')
+    marathon = {
+      'id': 'scale-logstash',
+      'cpus': 0.5,
+      'mem': 1024,
+      'disk': 256,
+      'instances': 1,
+      'container': {
+        'docker': {
+          'image': logstash_image,
+          'network': 'HOST',
+          'forcePullImage': True
         },
-        "type": "DOCKER",
-        "volumes": []
+        'type': 'DOCKER',
+        'volumes': []
       },
-      "portDefinitions": [
+      'portDefinitions': [
         {
-          "port": 12201,
-          "protocol": "udp"
+          'port': 12201,
+          'protocol': 'udp'
         }
       ],
-      "env": {
-          "ES_HOST": "%s"
-      },
-      "labels": {},
-      "healthChecks": [],
-      "uris": [%s]
+      'env': {},
+      'labels': {},
+      'healthChecks': [],
+      'uris': []
     }
-    ''' % (os.environ.get('LOGSTASH_DOCKER_IMAGE', 'logstash'), endpoints[0], config_uri)
 
-    r = requests.post('http://marathon.mesos:8080/v2/apps/', data=marathon)
+    # Capture any passed-in environment variables that are relevant to Logstash container.
+    CONFIG_URI = os.getenv('CONFIG_URI')
+    ELASTICSEARCH_URL = os.getenv('SCALE_ELASTICSEARCH_URL')
+    SLEEP_TIME = os.getenv('LOGSTASH_WATCHDOG_SLEEP_TIME')
+    TEMPLATE_URI = os.getenv('LOGSTASH_TEMPLATE_URI')
+
+    if ELASTICSEARCH_URL:
+        marathon['env']['ELASTICSEARCH_URL'] = ELASTICSEARCH_URL
+
+    if SLEEP_TIME:
+        marathon['env']['SLEEP_TIME'] = SLEEP_TIME
+
+    if TEMPLATE_URI:
+        marathon['env']['TEMPLATE_URI'] = TEMPLATE_URI
+
+    if CONFIG_URI:
+        marathon['uris'].append(CONFIG_URI)
+
+    r = requests.post('http://marathon.mesos:8080/v2/apps/', json=marathon)
+    if r.status_code >= 400:
+        print(r.text)
     while int(json.loads(requests.get('http://marathon.mesos:8080/v2/apps/scale-logstash').text)['app']['tasksRunning']) == 0:
         time.sleep(5)
     logstashPort = json.loads(requests.get('http://marathon.mesos:8080/v2/apps/scale-logstash').text)['app']['tasks'][0]['ports'][0]
     print('udp://scale-logstash.marathon.mesos:%s'%(logstashPort,))
-    print('http://%s' % endpoints[0])
 
 
 if __name__ == '__main__':
