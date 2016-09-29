9ec14a43fa52b62086d5521336d0aa03bb321e08
root <root@rdhpwngmp-01.711hpw.afrl-wrs.hpc.mil>
2016-09-29 11:19:31 -0400

    Added support for DCOS CLI

--- dockerfiles/framework/scale/entryPoint.sh
+++ dockerfiles/framework/scale/entryPoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -x
 
+exec ./dcos_cli.py > dcos_cli.log
+
 if [[ ${DCOS_PACKAGE_FRAMEWORK_NAME}x != x ]]
 then
     sed -i "s/framework.name\ =\ 'Scale'/framework.name\ =\ '"${DCOS_PACKAGE_FRAMEWORK_NAME}"'/" /opt/scale/scheduler/management/commands/scale_scheduler.py
@@ -15,36 +17,18 @@ then
   gosu root sed -i '/Listen 80/ aListen '${PORT0} /etc/httpd/conf/httpd.conf
 fi
 
-
 if [[ ${DEPLOY_DB} == 'true' ]]
 then
-    export SCALE_DB_PORT=$(./deployDb.py)
+    export SCALE_DB_PORT=`cat dcos_cli.log | grep DB_PORT | cut -d '=' -f2`
     echo "DATABASE_PORT: ${SCALE_DB_PORT}"
 fi
 echo "${SCALE_DB_HOST}:${SCALE_DB_PORT}:*:${SCALE_DB_USER}:${SCALE_DB_PASS}" >> ~/.pgpass
 chmod 0600 ~/.pgpass
 
-# wait for postgres database to spin up
-CHECK1="0"
-CHECK=$(/usr/bin/psql -U ${SCALE_DB_USER} -h ${SCALE_DB_HOST} -w -p ${SCALE_DB_PORT} &> /tmp/check; cat /tmp/check | head -1 | cut -d ':' -f 3)
-while [[ "$CHECK1" = "0" ]]; do
-  if [[ "$CHECK" = " Connection refused" ]]; then
-    sleep 2;
-    CHECK=$(/usr/bin/psql -U ${SCALE_DB_USER} -h ${SCALE_DB_HOST} -w -p ${SCALE_DB_PORT} &> /tmp/check; cat /tmp/check | head -1 | cut -d ':' -f 3);
-    echo ${CHECK};
-  elif [[ "$CHECK" = " No route to host" ]]; then
-   sleep 2;
-    CHECK=$(/usr/bin/psql -U ${SCALE_DB_USER} -h ${SCALE_DB_HOST} -w -p ${SCALE_DB_PORT} &> /tmp/check; cat /tmp/check | head -1 | cut -d ':' -f 3);
-    echo ${CHECK};
-  else
-    CHECK1="1"
-  fi
-done
-
 if [[ ${DEPLOY_LOGGING} == 'true' ]]
 then
-    IFS=$" " read -r SCALE_LOGGING_ADDRESS SCALE_ELASTICSEARCH_URL <<< $(./deployElk.py)
-    export SCALE_LOGGING_ADDRESS SCALE_ELASTICSEARCH_URL
+    export SCALE_LOGGING_ADDRESS=`cat dcos_cli.log | grep LOGGING_ADDRESS | cut -d '=' -f2`
+    export SCALE_ELASTICSEARCH_URL=`cat dcos_cli.log | grep ELASTICSEARCH_URL | cut -d '=' -f2`
     echo "LOGGING ADDRESS: ${SCALE_LOGGING_ADDRESS}"
     echo "ELASTICSEARCH URL: ${SCALE_ELASTICSEARCH_URL}"
 fi
