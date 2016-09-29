0050fa75cfc7e67d6a6f14bb5c9991e5316c1654
Chris Dickman <chris.dickman@appliedis.com>
2016-09-29 12:25:08 -0400

    Added superseded details to recipe details model

--- scale-ui/app/modules/recipes/models/RecipeDetails.js
+++ scale-ui/app/modules/recipes/models/RecipeDetails.js
@@ -2,7 +2,9 @@
     'use strict';
 
     angular.module('scaleApp').factory('RecipeDetails', function (RecipeData, RecipeTypeDefinition, RecipeType, RecipeTypeDetail, RecipeJobContainer, scaleConfig) {
-        var RecipeDetails = function (id, created, completed, last_modified, data, recipe_type, recipe_type_rev, jobs) {
+        var RecipeDetails = function (id, created, completed, last_modified, data, recipe_type, recipe_type_rev, jobs,
+                                      is_superseded, root_superseded_recipe, superseded_recipe, superseded_by_recipe,
+                                      superseded) {
             this.id = id;
             this.created = created;
             this.completed = completed;
@@ -12,6 +14,12 @@
             this.recipe_type = RecipeType.transformer(recipe_type);
             this.recipe_type_rev = RecipeTypeDetail.transformer(recipe_type_rev);
             this.jobs = RecipeJobContainer.transformer(jobs);
+            this.is_superseded = is_superseded;
+            this.root_superseded_recipe = root_superseded_recipe;
+            this.superseded_recipe = superseded_recipe;
+            this.superseded_by_recipe = superseded_by_recipe;
+            this.superseded = superseded;
+            this.superseded_formatted = this.superseded ? moment.utc(superseded).format(scaleConfig.dateFormats.day_second_utc) : this.superseded;
         };
 
         // static methods, assigned to class
@@ -25,7 +33,12 @@
                     data.data,
                     data.recipe_type,
                     data.recipe_type_rev,
-                    data.jobs
+                    data.jobs,
+                    data.is_superseded,
+                    data.root_superseded_recipe,
+                    data.superseded_recipe,
+                    data.superseded_by_recipe,
+                    data.superseded
                 );
             }
             return new RecipeDetails();
