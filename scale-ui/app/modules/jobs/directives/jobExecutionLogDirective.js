7cae57987e8c613f9b2cf384d8d52b1bd87235b3
ericsvendsen-mil <eric.svendsen.ctr@us.af.mil>
2016-10-14 16:12:18 -0400

    bugfix to prevent combined endpoint from being called without a started param after the log has started tailing; added logic to remove duplicate log entries; bugfix for static log generation

--- scale-ui/app/modules/jobs/directives/jobExecutionLogDirective.js
+++ scale-ui/app/modules/jobs/directives/jobExecutionLogDirective.js
@@ -1,8 +1,9 @@
 (function () {
     'use strict';
     
-    angular.module('scaleApp').controller('jobExecutionLogController', function ($scope, $location, $element, $timeout, jobExecutionService, stateService) {
-        var vm = this;
+    angular.module('scaleApp').controller('jobExecutionLogController', function ($scope, $location, $element, $timeout, scaleConfig, jobExecutionService, stateService) {
+        var vm = this,
+                 latestScaleOrderNum = 0;
         
         var initialize = function () {
             vm.forceScroll = true;
@@ -10,28 +11,41 @@
             vm.execLog = [];
             stateService.setLogArgs([]);
 
-            $scope.$watch('execution', function () {
+            // debounce this watch operation to prevent duplicate calls
+            // set the timer for 100ms less than the job exe log poll interval
+            $scope.$watch('execution', _.debounce(function () {
                 if ($scope.execution) {
                     vm.status = $scope.execution.status.toLowerCase();
                     console.log($scope.execution);
                     jobExecutionService.getLog($scope.execution.id).then(null, null, function (result) {
                         if (result.$resolved) {
-                            if (result.$status === 204) {
-                                stateService.setLogArgs([]);
-                            } else {
+                            if (result.$status !== 204) {
+                                // Content was returned, so add it to the log array
                                 // get difference of max scroll length and current scroll length.var  = result.data;
                                 var div = $($element[0]).find('.bash');
                                 vm.scrollDiff = (div.scrollTop() + div.prop('offsetHeight')) - div.prop('scrollHeight');
                                 if (vm.scrollDiff >= 0) {
                                     vm.forceScroll = true;
                                 }
+                                // concat new content and sort log array by timestamp and then by order num
                                 vm.execLog = _.sortBy(vm.execLog.concat(result.hits.hits), ['_source.@timestamp', '_source.scale_order_num']);
                                 if (vm.execLog && vm.execLog.length > 0) {
-                                    stateService.setLogArgs([
-                                        {
-                                            started: _.last(vm.execLog)._source['@timestamp']
-                                        }
-                                    ]);
+                                    var lastLog = _.last(vm.execLog)._source;
+                                    if (lastLog.scale_order_num !== latestScaleOrderNum) {
+                                        // new entries, so leave them on the array and report the new timestamp
+                                        console.log('New entries - ' + lastLog.scale_order_num + ' : ' + latestScaleOrderNum);
+                                        latestScaleOrderNum = lastLog.scale_order_num;
+                                        stateService.setLogArgs([
+                                            {
+                                                // started: moment.utc(lastLog['@timestamp']).subtract(1, 's').toISOString()
+                                                started: lastLog['@timestamp']
+                                            }
+                                        ]);
+                                    } else {
+                                        // duplicate entries, so remove them from the array
+                                        console.log('Duplicate entries');
+                                        vm.execLog = _.take(vm.execLog, vm.execLog.length - result.hits.hits.length);
+                                    }
                                 }
                             }
                         } else {
@@ -42,7 +56,7 @@
                         }
                     });
                 }
-            });
+            }, scaleConfig.pollIntervals.jobExecutionLog - 100));
             $scope.$watch('vm.execLog', function () {
                 if (vm.execLog) {
                     if (vm.forceScroll || vm.scrollDiff >= 0) {
