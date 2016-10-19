1789571ebcbe942961b5d5e3bc894264df7efc17
Chris Ward <cward@ball.com>
2016-10-19 12:05:06 -0400

    Fixed unit tests to use versioning for the product app.

--- scale/product/test/test_views.py
+++ scale/product/test/test_views.py
@@ -10,6 +10,7 @@ import job.test.utils as job_test_utils
 import product.test.utils as product_test_utils
 import source.test.utils as source_test_utils
 import storage.test.utils as storage_test_utils
+import util.rest as rest_util
 
 
 class TestProductsView(TestCase):
@@ -41,95 +42,95 @@ class TestProductsView(TestCase):
     def test_invalid_started(self):
         """Tests calling the product files view when the started parameter is invalid."""
 
-        url = '/products/?started=hello'
+        url = rest_util.get_url('/products/?started=hello')
         response = self.client.generic('GET', url)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.content)
 
     def test_missing_tz_started(self):
         """Tests calling the product files view when the started parameter is missing timezone."""
 
-        url = '/products/?started=1970-01-01T00:00:00'
+        url = rest_util.get_url('/products/?started=1970-01-01T00:00:00')
         response = self.client.generic('GET', url)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.content)
 
     def test_invalid_ended(self):
         """Tests calling the product files view when the ended parameter is invalid."""
 
-        url = '/products/?started=1970-01-01T00:00:00Z&ended=hello'
+        url = rest_util.get_url('/products/?started=1970-01-01T00:00:00Z&ended=hello')
         response = self.client.generic('GET', url)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.content)
 
     def test_missing_tz_ended(self):
         """Tests calling the product files view when the ended parameter is missing timezone."""
 
-        url = '/products/?started=1970-01-01T00:00:00Z&ended=1970-01-02T00:00:00'
+        url = rest_util.get_url('/products/?started=1970-01-01T00:00:00Z&ended=1970-01-02T00:00:00')
         response = self.client.generic('GET', url)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.content)
 
     def test_negative_time_range(self):
         """Tests calling the product files view with a negative time range."""
 
-        url = '/products/?started=1970-01-02T00:00:00Z&ended=1970-01-01T00:00:00'
+        url = rest_util.get_url('/products/?started=1970-01-02T00:00:00Z&ended=1970-01-01T00:00:00')
         response = self.client.generic('GET', url)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.content)
 
     def test_job_type_id(self):
         """Tests successfully calling the product files view filtered by job type identifier."""
 
-        url = '/products/?job_type_id=%s' % self.job_type1.id
+        url = rest_util.get_url('/products/?job_type_id=%s' % self.job_type1.id)
         response = self.client.generic('GET', url)
-        result = json.loads(response.content)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        result = json.loads(response.content)
         self.assertEqual(len(result['results']), 1)
         self.assertEqual(result['results'][0]['job_type']['id'], self.job_type1.id)
 
     def test_job_type_name(self):
         """Tests successfully calling the product files view filtered by job type name."""
 
-        url = '/products/?job_type_name=%s' % self.job_type1.name
+        url = rest_util.get_url('/products/?job_type_name=%s' % self.job_type1.name)
         response = self.client.generic('GET', url)
-        result = json.loads(response.content)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        result = json.loads(response.content)
         self.assertEqual(len(result['results']), 1)
         self.assertEqual(result['results'][0]['job_type']['name'], self.job_type1.name)
 
     def test_job_type_category(self):
         """Tests successfully calling the product files view filtered by job type category."""
 
-        url = '/products/?job_type_category=%s' % self.job_type1.category
+        url = rest_util.get_url('/products/?job_type_category=%s' % self.job_type1.category)
         response = self.client.generic('GET', url)
-        result = json.loads(response.content)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        result = json.loads(response.content)
         self.assertEqual(len(result['results']), 1)
         self.assertEqual(result['results'][0]['job_type']['category'], self.job_type1.category)
 
     def test_is_operational(self):
         """Tests successfully calling the product files view filtered by is_operational flag."""
 
-        url = '/products/?is_operational=true'
+        url = rest_util.get_url('/products/?is_operational=true')
         response = self.client.generic('GET', url)
-        result = json.loads(response.content)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        result = json.loads(response.content)
         self.assertEqual(len(result['results']), 1)
         self.assertEqual(result['results'][0]['job_type']['is_operational'], self.job_type1.is_operational)
 
     def test_is_published(self):
         """Tests successfully calling the product files view filtered by is_published flag."""
 
-        url = '/products/?is_published=false'
+        url = rest_util.get_url('/products/?is_published=false')
         response = self.client.generic('GET', url)
-        result = json.loads(response.content)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        result = json.loads(response.content)
         self.assertEqual(len(result['results']), 1)
         self.assertEqual(result['results'][0]['id'], self.product2a.id)
         self.assertFalse(result['results'][0]['is_published'])
@@ -137,22 +138,22 @@ class TestProductsView(TestCase):
     def test_file_name(self):
         """Tests successfully calling the product files view filtered by file name."""
 
-        url = '/products/?file_name=test.txt'
+        url = rest_util.get_url('/products/?file_name=test.txt')
         response = self.client.generic('GET', url)
-        result = json.loads(response.content)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        result = json.loads(response.content)
         self.assertEqual(len(result['results']), 1)
         self.assertEqual(result['results'][0]['file_name'], self.product1.file_name)
 
     def test_successful(self):
         """Tests successfully calling the product files view."""
 
-        url = '/products/'
+        url = rest_util.get_url('/products/')
         response = self.client.generic('GET', url)
-        result = json.loads(response.content)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        result = json.loads(response.content)
         self.assertEqual(len(result['results']), 2)
 
         for entry in result['results']:
@@ -185,12 +186,11 @@ class TestProductDetailsView(TestCase):
     def test_id(self):
         """Tests successfully calling the product files view by id."""
 
-        url = '/products/%i/' % self.product.id
+        url = rest_util.get_url('/products/%i/' % self.product.id)
         response = self.client.generic('GET', url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
+
         result = json.loads(response.content)
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
         self.assertEqual(result['id'], self.product.id)
         self.assertEqual(result['file_name'], self.product.file_name)
         self.assertEqual(result['job_type']['id'], self.product.job_type_id)
@@ -207,12 +207,11 @@ class TestProductDetailsView(TestCase):
     def test_file_name(self):
         """Tests successfully calling the product files view by file name."""
 
-        url = '/products/%s/' % self.product.file_name
+        url = rest_util.get_url('/products/%s/' % self.product.file_name)
         response = self.client.generic('GET', url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
+
         result = json.loads(response.content)
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
         self.assertEqual(result['id'], self.product.id)
         self.assertEqual(result['file_name'], self.product.file_name)
         self.assertEqual(result['job_type']['id'], self.product.job_type_id)
@@ -229,13 +228,13 @@ class TestProductDetailsView(TestCase):
     def test_missing(self):
         """Tests calling the source files view with an invalid id or file name."""
 
-        url = '/products/12345/'
+        url = rest_util.get_url('/products/12345/')
         response = self.client.generic('GET', url)
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND, response.content)
 
-        url = '/products/missing_file.txt/'
+        url = rest_util.get_url('/products/missing_file.txt/')
         response = self.client.generic('GET', url)
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND, response.content)
 
 
 class TestProductsUpdatesView(TestCase):
@@ -265,106 +264,106 @@ class TestProductsUpdatesView(TestCase):
     def test_invalid_started(self):
         """Tests calling the product file updates view when the started parameter is invalid."""
 
-        url = '/products/updates/?started=hello'
+        url = rest_util.get_url('/products/updates/?started=hello')
         response = self.client.generic('GET', url)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.content)
 
     def test_missing_tz_started(self):
         """Tests calling the product file updates view when the started parameter is missing timezone."""
 
-        url = '/products/updates/?started=1970-01-01T00:00:00'
+        url = rest_util.get_url('/products/updates/?started=1970-01-01T00:00:00')
         response = self.client.generic('GET', url)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.content)
 
     def test_invalid_ended(self):
         """Tests calling the product file updates view when the ended parameter is invalid."""
 
-        url = '/products/updates/?started=1970-01-01T00:00:00Z&ended=hello'
+        url = rest_util.get_url('/products/updates/?started=1970-01-01T00:00:00Z&ended=hello')
         response = self.client.generic('GET', url)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.content)
 
     def test_missing_tz_ended(self):
         """Tests calling the product file updates view when the ended parameter is missing timezone."""
 
-        url = '/products/updates/?started=1970-01-01T00:00:00Z&ended=1970-01-02T00:00:00'
+        url = rest_util.get_url('/products/updates/?started=1970-01-01T00:00:00Z&ended=1970-01-02T00:00:00')
         response = self.client.generic('GET', url)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.content)
 
     def test_negative_time_range(self):
         """Tests calling the product file updates view with a negative time range."""
 
-        url = '/products/updates/?started=1970-01-02T00:00:00Z&ended=1970-01-01T00:00:00'
+        url = rest_util.get_url('/products/updates/?started=1970-01-02T00:00:00Z&ended=1970-01-01T00:00:00')
         response = self.client.generic('GET', url)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.content)
 
     def test_job_type_id(self):
         """Tests successfully calling the product file updates view filtered by job type identifier."""
 
-        url = '/products/updates/?job_type_id=%s' % self.job_type1.id
+        url = rest_util.get_url('/products/updates/?job_type_id=%s' % self.job_type1.id)
         response = self.client.generic('GET', url)
-        result = json.loads(response.content)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        result = json.loads(response.content)
         self.assertEqual(len(result['results']), 1)
         self.assertEqual(result['results'][0]['job_type']['id'], self.job_type1.id)
 
     def test_job_type_name(self):
         """Tests successfully calling the product file updates view filtered by job type name."""
 
-        url = '/products/updates/?job_type_name=%s' % self.job_type1.name
+        url = rest_util.get_url('/products/updates/?job_type_name=%s' % self.job_type1.name)
         response = self.client.generic('GET', url)
-        result = json.loads(response.content)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        result = json.loads(response.content)
         self.assertEqual(len(result['results']), 1)
         self.assertEqual(result['results'][0]['job_type']['name'], self.job_type1.name)
 
     def test_job_type_category(self):
         """Tests successfully calling the product file updates view filtered by job type category."""
 
-        url = '/products/updates/?job_type_category=%s' % self.job_type1.category
+        url = rest_util.get_url('/products/updates/?job_type_category=%s' % self.job_type1.category)
         response = self.client.generic('GET', url)
-        result = json.loads(response.content)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        result = json.loads(response.content)
         self.assertEqual(len(result['results']), 1)
         self.assertEqual(result['results'][0]['job_type']['category'], self.job_type1.category)
 
     def test_is_operational(self):
         """Tests successfully calling the product file updates view filtered by is_operational flag."""
 
-        url = '/products/updates/?is_operational=true'
+        url = rest_util.get_url('/products/updates/?is_operational=true')
         response = self.client.generic('GET', url)
-        result = json.loads(response.content)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        result = json.loads(response.content)
         self.assertEqual(len(result['results']), 1)
         self.assertEqual(result['results'][0]['job_type']['is_operational'], self.job_type1.is_operational)
 
     def test_file_name(self):
         """Tests successfully calling the product file updates view filtered by file name."""
 
-        url = '/products/updates/?file_name=test.txt'
+        url = rest_util.get_url('/products/updates/?file_name=test.txt')
         response = self.client.generic('GET', url)
-        result = json.loads(response.content)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        result = json.loads(response.content)
         self.assertEqual(len(result['results']), 1)
         self.assertEqual(result['results'][0]['file_name'], self.product1.file_name)
 
     def test_successful(self):
         """Tests successfully calling the product file updates view."""
 
-        url = '/products/updates/'
+        url = rest_util.get_url('/products/updates/')
         response = self.client.generic('GET', url)
-        result = json.loads(response.content)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        result = json.loads(response.content)
         self.assertEqual(len(result['results']), 3)
 
         for entry in result['results']:
