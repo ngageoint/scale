4b6c09b381b1b731f9d7191703bbd5906f981881
Chris Ward <cward@ball.com>
2016-10-19 01:52:41 -0400

    Fixed unit tests to use versioning for the batch app.

--- scale/batch/test/test_views.py
+++ scale/batch/test/test_views.py
@@ -8,6 +8,7 @@ from rest_framework import status
 
 import recipe.test.utils as recipe_test_utils
 import batch.test.utils as batch_test_utils
+import util.rest as rest_util
 
 
 class TestBatchesView(TestCase):
@@ -26,7 +27,7 @@ class TestBatchesView(TestCase):
     def test_successful(self):
         """Tests successfully calling the batches view."""
 
-        url = '/batches/'
+        url = rest_util.get_url('/batches/')
         response = self.client.generic('GET', url)
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
@@ -45,7 +46,7 @@ class TestBatchesView(TestCase):
     def test_status(self):
         """Tests successfully calling the batches view filtered by status."""
 
-        url = '/batches/?status=SUBMITTED'
+        url = rest_util.get_url('/batches/?status=SUBMITTED')
         response = self.client.generic('GET', url)
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
@@ -56,7 +57,7 @@ class TestBatchesView(TestCase):
     def test_recipe_type_id(self):
         """Tests successfully calling the batches view filtered by recipe type identifier."""
 
-        url = '/batches/?recipe_type_id=%s' % self.batch1.recipe_type.id
+        url = rest_util.get_url('/batches/?recipe_type_id=%s' % self.batch1.recipe_type.id)
         response = self.client.generic('GET', url)
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
@@ -67,7 +68,7 @@ class TestBatchesView(TestCase):
     def test_recipe_type_name(self):
         """Tests successfully calling the batches view filtered by recipe type name."""
 
-        url = '/batches/?recipe_type_name=%s' % self.batch1.recipe_type.name
+        url = rest_util.get_url('/batches/?recipe_type_name=%s' % self.batch1.recipe_type.name)
         response = self.client.generic('GET', url)
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
@@ -84,7 +85,7 @@ class TestBatchesView(TestCase):
         recipe_type1c = recipe_test_utils.create_recipe_type(name='test1', version='3.0')
         batch_test_utils.create_batch(recipe_type=recipe_type1c)
 
-        url = '/batches/?order=recipe_type__name&order=-recipe_type__version'
+        url = rest_util.get_url('/batches/?order=recipe_type__name&order=-recipe_type__version')
         response = self.client.generic('GET', url)
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
@@ -109,7 +110,7 @@ class TestBatchDetailsView(TestCase):
     def test_not_found(self):
         """Tests successfully calling the get batch details view with a batch id that does not exist."""
 
-        url = '/batches/100/'
+        url = rest_util.get_url('/batches/100/')
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND, response.content)
@@ -117,7 +118,7 @@ class TestBatchDetailsView(TestCase):
     def test_successful(self):
         """Tests successfully calling the get batch details view."""
 
-        url = '/batches/%d/' % self.batch.id
+        url = rest_util.get_url('/batches/%d/' % self.batch.id)
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
