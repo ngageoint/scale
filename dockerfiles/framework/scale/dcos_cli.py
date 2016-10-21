d55acdcc1973b7f7bf775036d9a11fdf57d52349
Jonathan Meyer <jon@gisjedi.com>
2016-10-20 23:59:28 -0400

    Always remove logstash and webserver apps on scheduler restart.

--- dockerfiles/framework/scale/dcos_cli.py
+++ dockerfiles/framework/scale/dcos_cli.py
@@ -111,86 +111,84 @@ def wait_app_healthy(app_name):
 
 
 def deploy_webserver(app_name, es_urls, db_host, db_port):
-    # Check if scale-db is already running
-    if not check_app_exists(app_name):
-        # attempt to delete an old instance..if it doesn't exists it will error but we don't care so we ignore it
-        delete_marathon_app(app_name)
+    # attempt to delete an old instance..if it doesn't exists it will error but we don't care so we ignore it
+    delete_marathon_app(app_name)
 
-        vhost = os.getenv('SCALE_VHOST')
-        cpu = os.getenv('SCALE_WEBSERVER_CPU', 1)
-        memory = os.getenv('SCALE_WEBSERVER_MEMORY', 2048)
-        db_name = os.getenv('SCALE_DB_NAME', 'scale')
-        db_user = os.getenv('SCALE_DB_USER', 'scale')
-        db_pass = os.getenv('SCALE_DB_PASS', 'scale')
-        docker_image = os.getenv('MARATHON_APP_DOCKER_IMAGE')
-        optional_envs = ['SCALE_SECRET_KEY', 'SCALE_ALLOWED_HOSTS']
+    vhost = os.getenv('SCALE_VHOST')
+    cpu = os.getenv('SCALE_WEBSERVER_CPU', 1)
+    memory = os.getenv('SCALE_WEBSERVER_MEMORY', 2048)
+    db_name = os.getenv('SCALE_DB_NAME', 'scale')
+    db_user = os.getenv('SCALE_DB_USER', 'scale')
+    db_pass = os.getenv('SCALE_DB_PASS', 'scale')
+    docker_image = os.getenv('MARATHON_APP_DOCKER_IMAGE')
+    optional_envs = ['SCALE_SECRET_KEY', 'SCALE_ALLOWED_HOSTS']
 
-        marathon = {
-            'id': app_name,
-            'cpus': int(cpu),
-            'mem': int(memory),
-            'disk': 0,
-            'instances': 1,
-            'container': {
-                'docker': {
-                    'image': docker_image,
-                    'network': 'BRIDGE',
-                    'portMappings': [{
-                        'containerPort': 80,
-                        'hostPort': 0
-                    }
-                    ],
-                    'forcePullImage': True
-                },
-                'type': 'DOCKER'
+    marathon = {
+        'id': app_name,
+        'cpus': int(cpu),
+        'mem': int(memory),
+        'disk': 0,
+        'instances': 1,
+        'container': {
+            'docker': {
+                'image': docker_image,
+                'network': 'BRIDGE',
+                'portMappings': [{
+                    'containerPort': 80,
+                    'hostPort': 0
+                }
+                ],
+                'forcePullImage': True
             },
-            'env': {
-                "DCOS_PACKAGE_FRAMEWORK_NAME": FRAMEWORK_NAME,
-                "ENABLE_WEBSERVER": 'true',
-                "SCALE_DB_HOST": db_host,
-                "SCALE_DB_NAME": db_name,
-                "SCALE_DB_PORT": str(db_port),
-                "SCALE_DB_USER": db_user,
-                "SCALE_DB_PASS": db_pass,
-                "SCALE_STATIC_URL": "/service/%s/static/" % FRAMEWORK_NAME,
-                "SCALE_WEBSERVER_CPU": str(cpu),
-                "SCALE_WEBSERVER_MEMORY": str(memory),
-                "SCALE_ELASTICSEARCH_URLS": es_urls
+            'type': 'DOCKER'
+        },
+        'env': {
+            "DCOS_PACKAGE_FRAMEWORK_NAME": FRAMEWORK_NAME,
+            "ENABLE_WEBSERVER": 'true',
+            "SCALE_DB_HOST": db_host,
+            "SCALE_DB_NAME": db_name,
+            "SCALE_DB_PORT": str(db_port),
+            "SCALE_DB_USER": db_user,
+            "SCALE_DB_PASS": db_pass,
+            "SCALE_STATIC_URL": "/service/%s/static/" % FRAMEWORK_NAME,
+            "SCALE_WEBSERVER_CPU": str(cpu),
+            "SCALE_WEBSERVER_MEMORY": str(memory),
+            "SCALE_ELASTICSEARCH_URLS": es_urls
+        },
+        'labels': {
+            "DCOS_PACKAGE_FRAMEWORK_NAME": FRAMEWORK_NAME,
+            "HAPROXY_GROUP": "internal,external",
+            "DCOS_SERVICE_SCHEME": "http",
+            "DCOS_SERVICE_NAME": FRAMEWORK_NAME,
+            "DCOS_SERVICE_PORT_INDEX": "0",
+            "HAPROXY_0_VHOST": vhost
+        },
+        'healthChecks': [
+            {
+                "path": "/api/version",
+                "protocol": "TCP",
+                "gracePeriodSeconds": 300,
+                "intervalSeconds": 20,
+                "portIndex": 0,
+                "timeoutSeconds": 20,
+                "maxConsecutiveFailures": 3
             },
-            'labels': {
-                "DCOS_PACKAGE_FRAMEWORK_NAME": FRAMEWORK_NAME,
-                "HAPROXY_GROUP": "internal,external",
-                "DCOS_SERVICE_SCHEME": "http",
-                "DCOS_SERVICE_NAME": FRAMEWORK_NAME,
-                "DCOS_SERVICE_PORT_INDEX": "0",
-                "HAPROXY_0_VHOST": vhost
-            },
-            'healthChecks': [
-                {
-                    "path": "/api/version",
-                    "protocol": "TCP",
-                    "gracePeriodSeconds": 300,
-                    "intervalSeconds": 20,
-                    "portIndex": 0,
-                    "timeoutSeconds": 20,
-                    "maxConsecutiveFailures": 3
-                },
-            ],
-            'uris': []
-        }
-        CONFIG_URI = os.getenv('CONFIG_URI')
-        if CONFIG_URI:
-            marathon['uris'].append(CONFIG_URI)
-        # For all optional environment variable that are set pass them on.
-        for env in optional_envs:
-            env_value = os.getenv(env)
-            if env_value:
-                marathon['env'][env] = env_value
+        ],
+        'uris': []
+    }
+    CONFIG_URI = os.getenv('CONFIG_URI')
+    if CONFIG_URI:
+        marathon['uris'].append(CONFIG_URI)
+    # For all optional environment variable that are set pass them on.
+    for env in optional_envs:
+        env_value = os.getenv(env)
+        if env_value:
+            marathon['env'][env] = env_value
 
-        deploy_marathon_app(marathon)
-        wait_app_deploy(app_name)
-        time.sleep(5)
-        wait_app_healthy(app_name)
+    deploy_marathon_app(marathon)
+    wait_app_deploy(app_name)
+    time.sleep(5)
+    wait_app_healthy(app_name)
 
 
 def deploy_database(app_name):
@@ -266,78 +264,76 @@ def get_elasticsearch_urls():
 
 
 def deploy_logstash(app_name, es_urls):
-    if not check_app_exists(app_name):
-        # attempt to delete an old instance..if it doesn't exists it will error but we don't care so we ignore it
-        delete_marathon_app(app_name)
+    # attempt to delete an old instance..if it doesn't exists it will error but we don't care so we ignore it
+    delete_marathon_app(app_name)
 
-        # get the Logstash container API endpoints
-        logstash_image = os.getenv('LOGSTASH_DOCKER_IMAGE', 'geoint/logstash-elastic-ha')
-        marathon = {
-            'id': app_name,
-            'cpus': 0.5,
-            'mem': 1024,
-            'disk': 256,
-            'instances': 1,
-            'container': {
-                'docker': {
-                    'image': logstash_image,
-                    'network': 'BRIDGE',
-                    'portMappings': [{
-                        'containerPort': 8000,
-                        'hostPort': 9229,
+    # get the Logstash container API endpoints
+    logstash_image = os.getenv('LOGSTASH_DOCKER_IMAGE', 'geoint/logstash-elastic-ha')
+    marathon = {
+        'id': app_name,
+        'cpus': 0.5,
+        'mem': 1024,
+        'disk': 256,
+        'instances': 1,
+        'container': {
+            'docker': {
+                'image': logstash_image,
+                'network': 'BRIDGE',
+                'portMappings': [{
+                    'containerPort': 8000,
+                    'hostPort': 9229,
+                    'protocol': 'tcp'
+                },
+                    {
+                        'containerPort': 80,
+                        'hostPort': 0,
                         'protocol': 'tcp'
-                    },
-                        {
-                            'containerPort': 80,
-                            'hostPort': 0,
-                            'protocol': 'tcp'
-                        }
-                    ],
-                    'forcePullImage': True
-                },
-                'type': 'DOCKER',
-                'volumes': []
+                    }
+                ],
+                'forcePullImage': True
             },
-            'env': {
-                'LOGSTASH_ARGS': '-w 1',
-                'ELASTICSEARCH_URLS': es_urls
+            'type': 'DOCKER',
+            'volumes': []
+        },
+        'env': {
+            'LOGSTASH_ARGS': '-w 1',
+            'ELASTICSEARCH_URLS': es_urls
+        },
+        'labels': {},
+        'healthChecks': [
+            {
+                "protocol": "HTTP",
+                "path": "/",
+                "gracePeriodSeconds": 5,
+                "intervalSeconds": 10,
+                "portIndex": 1,
+                "timeoutSeconds": 10,
+                "maxConsecutiveFailures": 3
             },
-            'labels': {},
-            'healthChecks': [
-                {
-                    "protocol": "HTTP",
-                    "path": "/",
-                    "gracePeriodSeconds": 5,
-                    "intervalSeconds": 10,
-                    "portIndex": 1,
-                    "timeoutSeconds": 10,
-                    "maxConsecutiveFailures": 3
-                },
-            ],
-            'uris': []
-        }
+        ],
+        'uris': []
+    }
 
-        # Capture any passed-in environment variables that are relevant to Logstash container.
-        CONFIG_URI = os.getenv('CONFIG_URI')
-        SLEEP_TIME = os.getenv('LOGSTASH_WATCHDOG_SLEEP_TIME')
-        TEMPLATE_URI = os.getenv('LOGSTASH_TEMPLATE_URI')
-        if SLEEP_TIME:
-            marathon['env']['SLEEP_TIME'] = SLEEP_TIME
-        if TEMPLATE_URI:
-            marathon['env']['TEMPLATE_URI'] = TEMPLATE_URI
-        if CONFIG_URI:
-            marathon['uris'].append(CONFIG_URI)
+    # Capture any passed-in environment variables that are relevant to Logstash container.
+    CONFIG_URI = os.getenv('CONFIG_URI')
+    SLEEP_TIME = os.getenv('LOGSTASH_WATCHDOG_SLEEP_TIME')
+    TEMPLATE_URI = os.getenv('LOGSTASH_TEMPLATE_URI')
+    if SLEEP_TIME:
+        marathon['env']['SLEEP_TIME'] = SLEEP_TIME
+    if TEMPLATE_URI:
+        marathon['env']['TEMPLATE_URI'] = TEMPLATE_URI
+    if CONFIG_URI:
+        marathon['uris'].append(CONFIG_URI)
 
-        deploy_marathon_app(marathon)
+    deploy_marathon_app(marathon)
 
-        wait_app_deploy(app_name)
-        time.sleep(5)
-        wait_app_healthy(app_name)
+    wait_app_deploy(app_name)
+    time.sleep(5)
+    wait_app_healthy(app_name)
 
     logstash_port = get_marathon_port(app_name, 0)
 
     return logstash_port
-
 
 
 if __name__ == '__main__':
