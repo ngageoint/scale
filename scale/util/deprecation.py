c5205258313e53719b41809debd9ba00a3dce93c
Jonathan Meyer <jon@gisjedi.com>
2017-03-17 17:56:26 +0000

    Added missing import for JSONField

--- scale/util/deprecation.py
+++ scale/util/deprecation.py
@@ -1,5 +1,8 @@
 # Classes needed to shim migrations based on abandoned external packages
 
+from django.contrib.postgres.fields import JSONField
+
+
 class JSONStringField(JSONField):
     
     def db_type(self, connection):
