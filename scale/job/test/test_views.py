f1679057e9a7c9c2f2a05c9d9c22ac74f8522922
Jonathan Meyer <jon@gisjedi.com>
2017-03-21 19:55:36 -0400

    Fixed remaining unit tests.

--- scale/job/test/test_views.py
+++ scale/job/test/test_views.py
@@ -758,7 +758,7 @@ class TestJobTypesView(TestCase):
         results = json.loads(response.content)
         self.assertEqual(results['id'], job_type.id)
         self.assertIsNotNone(results['interface'])
-        self.assertDictEqual(results['error_mapping']['exit_codes'], {})
+        self.assertIsNone(results['error_mapping'])
         self.assertEqual(results['trigger_rule']['type'], 'PARSE')
 
     def test_create_missing_param(self):
