33835ba903ec8d5f5c8710658e5747a929bcb25e
Jonathan Meyer <jon@gisjedi.com>
2017-04-14 05:54:02 -0400

    Updated win scripts to correct PostGIS and django init

--- scale/environment/win-init.bat
+++ scale/environment/win-init.bat
@@ -7,28 +7,28 @@ docker run -d --restart=always -p %SCALE_DB_PORT%:5432 --name scale-postgis -e P
 REM Configure database
 echo "Giving Postgres a moment to start up before initializing..."
 ping 127.0.0.1 -n 6 > nul
-echo "CREATE USER scale PASSWORD 'scale' SUPERUSER;" >> database-commands.sql
-echo "CREATE DATABASE scale OWNER=scale;" >> database-commands.sql
+echo CREATE USER scale PASSWORD 'scale' SUPERUSER; >> database-commands.sql
+echo CREATE DATABASE scale OWNER=scale; >> database-commands.sql
 docker cp database-commands.sql scale-postgis:/database-commands.sql
 del database-commands.sql
-docker exec -it scale-postgis su postgres -c 'psql -f /database-commands.sql'
-docker exec -it scale-postgis su postgres -c 'psql scale -c "CREATE EXTENSION postgis;"'
+docker exec -it scale-postgis psql -U postgres -f /database-commands.sql
+docker exec -it scale-postgis psql -U scale -c "CREATE EXTENSION postgis;" scale
 
 copy scale\local_settings_dev.py scale\local_settings.py
 REM Set default connection string for database
-echo POSTGIS_TEMPLATE = 'template_postgis' >> scale/local_settings.py
-echo. >> scale/local_settings.py
-echo DATABASES = { >> scale/local_settings.py
-echo     'default': { >> scale/local_settings.py
-echo         'ENGINE': 'django.contrib.gis.db.backends.postgis', >> scale/local_settings.py
-echo         'NAME': 'scale', >> scale/local_settings.py
-echo         'USER': 'scale', >> scale/local_settings.py
-echo         'PASSWORD': 'scale', >> scale/local_settings.py
-echo         'HOST': 'localhost', >> scale/local_settings.py
-echo         'PORT': '%SCALE_DB_PORT%', >> scale/local_settings.py
-echo         'TEST': {'NAME': 'test_scale'}, >> scale/local_settings.py
-echo     }, >> scale/local_settings.py
-echo } >> scale/local_settings.py
+echo POSTGIS_TEMPLATE = 'template_postgis' >> scale\local_settings.py
+echo. >> scale\local_settings.py
+echo DATABASES = { >> scale\local_settings.py
+echo     'default': { >> scale\local_settings.py
+echo         'ENGINE': 'django.contrib.gis.db.backends.postgis', >> scale\local_settings.py
+echo         'NAME': 'scale', >> scale\local_settings.py
+echo         'USER': 'scale', >> scale\local_settings.py
+echo         'PASSWORD': 'scale', >> scale\local_settings.py
+echo         'HOST': 'localhost', >> scale\local_settings.py
+echo         'PORT': '%SCALE_DB_PORT%', >> scale\local_settings.py
+echo         'TEST': {'NAME': 'test_scale'}, >> scale\local_settings.py
+echo     }, >> scale\local_settings.py
+echo } >> scale\local_settings.py
 
 REM Initialize virtual environment
 virtualenv environment\scale
