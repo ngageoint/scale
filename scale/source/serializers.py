9de342d590daa8dffcb7bdc94b65ee488046b648
Mike Holt <mholt1@ball.com>
2017-06-15 14:03:03 -0400

    Removed unsupported fields

--- scale/source/serializers.py
+++ scale/source/serializers.py
@@ -10,9 +10,6 @@ class SourceFileBaseSerializer(ScaleFileBaseSerializer):
     is_parsed = serializers.BooleanField()
     parsed = serializers.DateTimeField()
 
-    is_superseded = serializers.BooleanField()
-    superseded = serializers.DateTimeField()
-
 
 class SourceFileSerializer(SourceFileBaseSerializer):
     """Converts source file model fields to REST output"""
