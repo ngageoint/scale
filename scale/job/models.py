de27c00bb6bdf1cc3ca2602fcd84b863ba3746bd
Mike Holt <mholt1@ball.com>
2017-06-23 15:51:29 -0400

    reworked JobInputFile population step

--- scale/job/models.py
+++ scale/job/models.py
@@ -469,16 +469,15 @@ class JobManager(models.Manager):
         job.disk_out_required = disk_out_required
         job.last_modified = modified
 
-        # Query JobInputFile model and if empty, populate the model
-        if not JobInputFile.objects.filter(job__id=job.id).values('input_file'):
-            job_inputs = []
-            job_input = JobInputFile()
-            job_input.job_id = job.id
-            for input_file in input_files:           
-                job_input.input_file_id = input_file.id
-                job_input.job_input = input_file.name
-                job_inputs.append(job_input)
-            JobInputFile.objects.bulk_create(job_inputs)
+        # Configure and populate JobInputFile
+        job_inputs = []
+        job_input = JobInputFile()
+        job_input.job_id = job.id
+        for input_file in data.get_input_file_info():           
+            job_input.input_file_id = input_file(0)
+            job_input.job_input = input_file(1)
+            job_inputs.append(job_input)
+        JobInputFile.objects.bulk_create(job_inputs)
 
         # Update job model in database with single query
         self.filter(id=job.id).update(data=data.get_dict(), configuration=configuration.get_dict(),
