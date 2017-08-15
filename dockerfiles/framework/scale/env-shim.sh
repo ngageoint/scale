866d855a2f7c394183303718748dd9124404b4b7
Mike Holt <mike.n.holt@gmail.com>
2017-08-15 15:34:09 -0400

    modified rmp command

--- dockerfiles/framework/scale/env-shim.sh
+++ dockerfiles/framework/scale/env-shim.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 # Place any environment specific configuration below
-RUN rpm -ivh https://kojipkgs.fedoraproject.org//packages/http-parser/2.7.1/3.el7/x86_64/http-parser-2.7.1-3.el7.x86_64.rpm
+rpm -ivh https://kojipkgs.fedoraproject.org//packages/http-parser/2.7.1/3.el7/x86_64/http-parser-2.7.1-3.el7.x86_64.rpm
