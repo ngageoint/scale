811f52c4ae9ac10e3aa3724ea0d334ae689f3813
Derick Faller <derick.faller.ctr@us.af.mil>
2017-09-06 16:43:53 +0000

    Another bug fix

--- scale/queue/models.py
+++ scale/queue/models.py
@@ -650,7 +650,7 @@ class Queue(models.Model):
         :rtype: :class:`job.configuration.json.execution.exe_config.ExecutionConfiguration`
         """
 
-        return ExecutionConfiguration(self.configuration, validate=False)
+        return ExecutionConfiguration(self.configuration, do_validate=False)
 
     def get_job_interface(self):
         """Returns the interface for this queued job
@@ -659,7 +659,7 @@ class Queue(models.Model):
         :rtype: :class:`job.configuration.interface.job_interface.JobInterface`
         """
 
-        return JobInterface(self.interface, validate=False)
+        return JobInterface(self.interface, do_validate=False)
 
     def get_resources(self):
         """Returns the resources required by this queued job
@@ -668,7 +668,7 @@ class Queue(models.Model):
         :rtype: :class:`node.resources.node_resources.NodeResources`
         """
 
-        return Resources(self.resources, validate=False).get_node_resources()
+        return Resources(self.resources, do_validate=False).get_node_resources()
 
     class Meta(object):
         """meta information for the db"""
