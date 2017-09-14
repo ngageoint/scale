f606cd4011b7e58d3905f741d6b0c37f1ea6ffe5
Derick Faller <derick.faller.ctr@us.af.mil>
2017-09-14 15:54:06 +0000

    Added unit test for generating task info in status JSON

--- scale/job/test/tasks/test_manager.py
+++ scale/job/test/tasks/test_manager.py
@@ -5,12 +5,61 @@ import datetime
 import django
 from django.test import TestCase
 from django.utils.timezone import now
-from job.tasks.base_task import Task
 
 import job.test.utils as job_test_utils
+from job.tasks.base_task import Task
 from job.tasks.manager import TaskManager
+from job.tasks.node_task import NodeTask
 from job.tasks.update import TaskStatusUpdate
 from node.resources.node_resources import NodeResources
+
+
+# Non-abstract class to test implementation of base NodeTask class
+class ImplementedNodeTask(NodeTask):
+
+    def __init__(self, task_id, task_name, agent_id):
+        """Constructor
+        """
+
+        super(ImplementedNodeTask, self).__init__(task_id, task_name, agent_id)
+
+        self.task_type = 'impl-node-task'
+        self.title = 'Implemented Node Task'
+        self.description = 'Test description'
+
+    def get_resources(self):
+        """Returns the resources that are required/have been scheduled for this task
+
+        :returns: The scheduled resources for this task
+        :rtype: :class:`node.resources.node_resources.NodeResources`
+        """
+
+        return NodeResources()
+
+
+# Non-abstract class to test implementation of base SystemTask class
+from scheduler.tasks.system_task import SystemTask
+class ImplementedSystemTask(SystemTask):
+
+    def __init__(self, task_id, task_name, agent_id):
+        """Constructor
+        """
+
+        super(ImplementedSystemTask, self).__init__(task_id, task_name)
+
+        self.agent_id = agent_id
+        self.task_type = 'impl-system-task'
+        self.title = 'Implemented System Task'
+        self.description = 'Test description'
+
+    def get_resources(self):
+        """Returns the resources that are required/have been scheduled for this task
+
+        :returns: The scheduled resources for this task
+        :rtype: :class:`node.resources.node_resources.NodeResources`
+        """
+
+        return NodeResources()
 
 
 # Non-abstract class to test implementation of base Task class
@@ -31,6 +80,41 @@ class TestTaskManager(TestCase):
 
     def setUp(self):
         django.setup()
+
+    def test_generate_status_json(self):
+        """Tests calling TaskManager.generate_status_json()"""
+
+        task_id = 'task_1'
+        task_name = 'My Task'
+        agent_id = 'agent_1'
+        task_1 = ImplementedNodeTask(task_id, task_name, agent_id)
+
+        task_id = 'task_2'
+        task_name = 'My Task'
+        agent_id = 'agent_1'
+        task_2 = ImplementedNodeTask(task_id, task_name, agent_id)
+
+        task_id = 'task_3'
+        task_name = 'My Task'
+        agent_id = 'agent_1'
+        task_3 = ImplementedSystemTask(task_id, task_name, agent_id)
+
+        task_id = 'task_4'
+        task_name = 'My Task'
+        agent_id = 'agent_1'
+        task_4 = ImplementedSystemTask(task_id, task_name, agent_id)
+
+        when = now()
+        manager = TaskManager()
+        manager.launch_tasks([task_1, task_2, task_3, task_4], when)
+
+        nodes_list = [{'agent_id': 'agent_1'}]
+        manager.generate_status_json(nodes_list)
+
+        self.assertEqual(nodes_list[0]['node_tasks'][0]['type'], 'impl-node-task')
+        self.assertEqual(nodes_list[0]['node_tasks'][0]['count'], 2)
+        self.assertEqual(nodes_list[0]['system_tasks'][0]['type'], 'impl-system-task')
+        self.assertEqual(nodes_list[0]['system_tasks'][0]['count'], 2)
 
     def test_handle_task_update(self):
         """Tests calling TaskManager.handle_task_update()"""
