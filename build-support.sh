2afb17207a99e264612402d7a3ee41eb4c7365cf
Jonathan Meyer <jon@gisjedi.com>
2017-11-22 16:28:59 +0000

    Fixed various build issues with missing environment variables.

--- build-support.sh
+++ build-support.sh
@@ -6,6 +6,6 @@ COMPONENT=$2
 
 IMAGE_URL=${REGISTRY}/${DOCKER_USER}/${IMAGE_PREFIX}-${COMPONENT}:${CI_BUILD_TAG}
 
-docker build -t ${IMAGE_URL} --build-arg IMAGE=${BASE_IMAGE} dockerfiles/${COMPONENT}
+docker build -t ${IMAGE_URL} --build-arg VAULT_ZIP=${VAULT_ZIP} --build-arg IMAGE=${BASE_IMAGE} dockerfiles/${COMPONENT}
 docker push ${IMAGE_URL}
 
