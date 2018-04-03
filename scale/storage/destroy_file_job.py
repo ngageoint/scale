3d033de29a0970337b0af1c0aa7444201c9ff84a
Mike Holt <mike.n.holt@gmail.com>
2018-04-03 14:56:38 -0400

    reworked logic

--- scale/storage/destroy_file_job.py
+++ scale/storage/destroy_file_job.py
@@ -9,25 +9,26 @@ import sys
 logger = logging.getLogger(__name__)
 
 
-def destroy_file(file_path, job_id):
+def destroy_file(files, job_id, volume_path, broker):
     """Deletes the given file within a workspace.
 
-    :param file_path: The absolute path of the file to delete
-    :type file_path: string
+    :param files: List of dicts containing path and ID of the file to delete.
+    :type files: [collections.namedtuple]
     :param job_id: The ID of the job associated with the file
     :type job_id: int
+    :param volume_path: Absolute path to the local container location onto which the volume file system was mounted,
+            None if this broker does not use a container volume
+    :type volume_path: string
+    :param workspace: The storage broker
+    :type workspace: `storage.brokers.broker.Broker`
     """
 
-    if os.path.exists(file_path):
-        logger.info('Deleting %s', file_path)
-        try:
-            os.remove(file_path)
-        except:
-            logger.exception('There was an error when trying to delete %s', file_path)
-            sys.exit(10)
-    else:
-        logger.error('No file exists at %s', file_path)
-        sys.exit(20)
+    logger.info('Deleting %i files', len(files))
+    try:
+        broker.delete_file(volume_path=volume_path, files=files, update_model=False)
+    except:
+        logger.exception('There was an error when trying to delete files for job %i', job_id)
+        sys.exit(10)
 
-    logger.info('The file located at %s was deleted for job %i', file_path, job_id)
-    sys.exit(0)
+    logger.info('A file associated with job %i was deleted', job_id)
+    return
