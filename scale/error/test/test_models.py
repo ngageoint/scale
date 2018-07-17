c42f648241fc297298eb0ac802e651bb8c7fee3a
Derick Faller <derick.faller.ctr@us.af.mil>
2018-07-17 19:53:52 +0000

    Fixed error caching bug

--- scale/error/test/test_models.py
+++ scale/error/test/test_models.py
@@ -1,7 +1,29 @@
 import django
 from django.test import TestCase
 
-from error.models import Error
+from error.models import get_job_error, Error
+
+
+class Test_get_job_error(TestCase):
+
+    def setUp(self):
+        django.setup()
+
+    def test_get_job_error(self):
+        """Tests successfully calling get_job_error()"""
+
+        job_type_name = 'job_type_1'
+        error_model = Error()
+        error_model.name = 'error_1'
+        error_model.job_type_name = job_type_name
+        error_model.title = 'Error 1'
+        error_model.description = 'This is a description'
+        error_model.category = 'ALGORITHM'
+        Error.objects.save_job_error_models(job_type_name, [error_model])
+
+        # Test retrieving error model, which will load it into the cache
+        error_model = get_job_error(job_type_name, 'error_1')
+        self.assertEqual(error_model.name, 'error_1')
 
 
 class TestErrorManager(TestCase):
