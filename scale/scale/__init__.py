996e7b449b311ccdf4c7324189937b2a22f8c7d2
Derick Faller <derick.faller.ctr@us.af.mil>
2018-08-16 18:10:47 +0000

    Update snapshot revision to 5.6.0

--- scale/scale/__init__.py
+++ scale/scale/__init__.py
@@ -9,7 +9,7 @@ version_info_t = namedtuple(
     'version_info_t', ('major', 'minor', 'patch', 'qualifier'),
 )
 
-VERSION = version_info_t(5, 5, 0, '-snapshot')
+VERSION = version_info_t(5, 6, 0, '-snapshot')
 
 __version__ = '{0.major}.{0.minor}.{0.patch}{0.qualifier}'.format(VERSION)
 
