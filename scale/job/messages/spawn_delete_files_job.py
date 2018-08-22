f3f6df04895ecac639ee61901e2b5ffc924e5202
Mike Holt <mike.n.holt@gmail.com>
2018-08-22 14:48:17 -0400

    added logic - working

--- scale/job/messages/spawn_delete_files_job.py
+++ scale/job/messages/spawn_delete_files_job.py
@@ -5,7 +5,10 @@ import logging
 
 from django.db import transaction
 
+from data.data.value import JsonValue
+from job.messages.create_jobs import create_jobs_message
 from messaging.messages.message import CommandMessage
+from storage.models import ScaleFile
 
 
 logger = logging.getLogger(__name__)
@@ -61,4 +64,37 @@ class SpawnDeleteFilesJob(CommandMessage):
         """See :meth:`messaging.messages.message.CommandMessage.execute`
         """
 
+        # This message will look at the job's files and create and 
+        # queue a system job to delete those files in the workspace 
+        # (passing along the purge flag). This messages does not affect 
+        # any database models.
+        files_to_delete = ScaleFile.objects.filter_files(job_ids=[self.job_id])
+        # data.data.~ -> JobData -> dict -> create_job message
+        if files_to_delete:
+            mappings = self._build_mappings(files_to_delete)
+            
+            # Construct input data list
+            input_data = [JsonValue('job_id', self.job_id),
+                          JsonValue('purge', self.purge)]
+            for f in mappings['files']:
+                input_data.append(JsonValue('file', f))
+            for w in mappings['workspaces']:
+                input_data.append(JsonValue('workspaces', w))
+
+            print input_data
         return True
+
+    def _build_mappings(self, files_to_delete):
+        """Parses the files that are to be deleted and builds a mapping for
+        files and workspaces that will be used as the job input.
+
+        :param files_to_delete: The files to be deleted
+        :type files_to_delete: :class:`django.db.models.QuerySet`
+        :return: A mapping of the files and workspaces
+        :rtype: dict
+        """
+
+        files = [{'id': f.id, 'file_path': f.file_path, 'workspace': f.workspace.name} for f in files_to_delete]
+        workspaces = set([{f.workspace.name: f.workspace} for f in files_to_delete])
+
+        return {'files': files, 'workspaces': workspaces}
