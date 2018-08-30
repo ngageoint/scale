16ecc3136e4ff80162854631990029b8d34478fa
John Tobe <jtobe@ball.com>
2018-08-30 20:28:47 +0000

    Partial check-in

--- scale/job/views.py
+++ scale/job/views.py
@@ -1179,6 +1179,42 @@ class CancelJobsView(GenericAPIView):
         CommandMessageManager().send_messages([msg])
 
         return Response(status=status.HTTP_202_ACCEPTED)
+        
+    def _post_v5(self, request):
+        """Submit command message to cancel jobs that fit the given filter criteria
+
+        :param request: the HTTP GET request
+        :type request: :class:`rest_framework.request.Request`
+        :returns: the HTTP response to send back to the user
+        """
+
+        started = rest_util.parse_timestamp(request, 'started', required=False)
+        ended = rest_util.parse_timestamp(request, 'ended', required=False)
+        rest_util.check_time_range(started, ended)
+
+        error_categories = rest_util.parse_string_list(request, 'error_categories', required=False)
+        error_ids = rest_util.parse_int_list(request, 'error_ids', required=False)
+        job_ids = rest_util.parse_int_list(request, 'job_ids', required=False)
+        job_status = rest_util.parse_string(request, 'status', required=False)
+        job_type_ids = rest_util.parse_int_list(request, 'job_type_ids', required=False)
+        
+        statuses = rest_util.parse_string_list(request, 'status', required=False)
+        job_ids = rest_util.parse_int_list(request, 'job_id', required=False)
+        job_type_ids = rest_util.parse_int_list(request, 'job_type_id', required=False)
+        job_type_names = rest_util.parse_string_list(request, 'job_type_name', required=False)
+        batch_ids = rest_util.parse_int_list(request, 'batch_id', required=False)
+        recipe_ids = rest_util.parse_int_list(request, 'recipe_id', required=False)
+        error_categories = rest_util.parse_string_list(request, 'error_category', required=False)
+        error_ids = rest_util.parse_int_list(request, 'error_id', required=False)
+        is_superseded = rest_util.parse_bool(request, 'is_superseded', required=False)
+
+        # Create and send message
+        msg = create_cancel_jobs_bulk_message(started=started, ended=ended, error_categories=error_categories,
+                                              error_ids=error_ids, job_ids=job_ids, job_type_ids=job_type_ids,
+                                              status=job_status)
+        CommandMessageManager().send_messages([msg])
+
+        return Response(status=status.HTTP_202_ACCEPTED)
 
 
 class RequeueJobsView(GenericAPIView):
