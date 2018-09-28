0ab30fdd7a5d1a4fee48b903a426c97e7a24ec00
Mike Holt <mike.n.holt@gmail.com>
2018-09-28 11:24:09 -0400

    :white_check_mark: adding tests for purge-source

--- scale/storage/test/test_views.py
+++ scale/storage/test/test_views.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 import datetime as dt
 import json
+from mock import patch
 
 import django
 from django.test import TestCase
@@ -14,6 +15,8 @@ import product.test.utils as product_test_utils
 import recipe.test.utils as recipe_test_utils
 import storage.test.utils as storage_test_utils
 import util.rest as rest_util
+
+from storage.messages import PurgeSourceFile
 from storage.models import Workspace
 
 
@@ -1072,3 +1075,105 @@ class TestWorkspacesValidationViewV6(TestCase):
         results = json.loads(response.content)
         self.assertEqual(len(results['warnings']), 1)
         self.assertEqual(results['warnings'][0]['name'], 'broker_type')
+
+
+class TestPurgeSourceFileView(TestCase):
+    """Tests related to the purge source endpoint"""
+
+    api = 'v6'
+
+    def setUp(self):
+        django.setup()
+
+    @patch('storage.views.CommandMessageManager')
+    @patch('storage.views.create_purge_source_file_message')
+    def test_successful(self, mock_create, mock_msg_mgr):
+        """Tests purging a source file."""
+
+        msg = PurgeSourceFile()
+        mock_create.return_value = msg
+
+        input_file = storage_test_utils.create_file(file_type='SOURCE')
+
+        json_data = {
+            'file_id': input_file.id
+        }
+
+        url = '/%s/purge-source/' % self.api
+        response = self.client.generic('POST', url, json.dumps(json_data), 'application/json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT, response.content)
+        # Check that create_batch_recipes message was created and sent
+        mock_create.assert_called_with(input_file.id, )
+
+    # def test_missing_configuration(self):
+    #     """Tests validating a new workspace with missing configuration."""
+
+    #     json_data = {
+    #         'title': 'Workspace Title',
+    #         'description': 'Workspace description',
+    #     }
+
+    #     url = '/%s/workspaces/validation/' % self.api
+    #     response = self.client.generic('POST', url, json.dumps(json_data), 'application/json')
+
+    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.content)
+
+    # def test_configuration_bad_type(self):
+    #     """Tests validating a new workspace with configuration that is not a dict."""
+
+    #     json_data = {
+    #         'title': 'Workspace Title',
+    #         'description': 'Workspace description',
+    #         'configuration': 123,
+    #     }
+
+    #     url = '/%s/workspaces/validation/' % self.api
+    #     response = self.client.generic('POST', url, json.dumps(json_data), 'application/json')
+
+    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.content)
+
+    # def test_invalid_configuration(self):
+    #     """Tests validating a new workspace with invalid configuration."""
+
+    #     json_data = {
+    #         'title': 'Workspace Title',
+    #         'description': 'Workspace description',
+    #         'configuration': {
+    #             'broker': 123,
+    #         },
+    #     }
+
+    #     url = '/%s/workspaces/validation/' % self.api
+    #     response = self.client.generic('POST', url, json.dumps(json_data), 'application/json')
+
+    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.content)
+
+    # def test_warnings(self):
+    #     """Tests validating a new workspace where the broker type is changed."""
+
+    #     json_config = {
+    #         'broker': {
+    #             'type': 'host',
+    #             'host_path': '/host/path',
+    #         },
+    #     }
+    #     storage_test_utils.create_workspace(name='ws-test', json_config=json_config)
+
+    #     json_data = {
+    #         'name': 'ws-test',
+    #         'title': 'ws test',
+    #         'configuration': {
+    #             'broker': {
+    #                 'type': 'nfs',
+    #                 'nfs_path': 'host:/dir',
+    #             },
+    #         },
+    #     }
+
+    #     url = '/%s/workspaces/validation/' % self.api
+    #     response = self.client.generic('POST', url, json.dumps(json_data), 'application/json')
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
+
+    #     results = json.loads(response.content)
+    #     self.assertEqual(len(results['warnings']), 1)
+    #     self.assertEqual(results['warnings'][0]['name'], 'broker_type')
