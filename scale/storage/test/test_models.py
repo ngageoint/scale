238db415164b3971965a61be1eb4c6d18556ae93
Mike Holt <mike.n.holt@gmail.com>
2018-10-04 11:12:29 -0400

    :white_check_mark: added PurgeResults tests

--- scale/storage/test/test_models.py
+++ scale/storage/test/test_models.py
@@ -14,9 +14,10 @@ from mock import MagicMock, patch
 import storage.test.utils as storage_test_utils
 from storage.brokers.broker import FileDownload, FileMove, FileUpload
 from storage.exceptions import ArchivedWorkspace, DeletedFile, InvalidDataTypeTag
-from storage.models import CountryData, ScaleFile, Workspace
+from storage.models import CountryData, PurgeResults, ScaleFile, Workspace
 from storage.brokers.exceptions import InvalidBrokerConfiguration
 from storage.configuration.json.workspace_config_v6 import WorkspaceConfigurationV6
+from trigger.test import utils as trigger_test_utils
 
 
 class TestScaleFileUpdateUUID(TestCase):
@@ -501,11 +502,11 @@ class TestCountryData(TestCase):
         self.assertEqual(tmp[0].iso_num, 42)
         self.assertEqual(tmp[0].border, self.testborder)
         self.assertEqual(tmp[0].effective, self.testeffective)
-    
+
     def test_not_found(self):
         tmp = CountryData.objects.filter(name='Kerblekistan')
         self.assertEqual(tmp.count(), 0)
-    
+
     def test_border_update(self):
         newborder = geos.Polygon(((0, 0), (42, 0), (42, 42), (0, 42), (0, 0)))
         neweffective = datetime.datetime(2010, 4, 5, 18, 26, 0, tzinfo=utc)
@@ -518,6 +519,26 @@ class TestCountryData(TestCase):
     def test_border_update_not_found(self):
         newborder = geos.Polygon(((0, 0), (42, 0), (42, 42), (0, 42), (0, 0)))
         self.assertRaises(CountryData.DoesNotExist, CountryData.objects.update_border, 'Kerblekistan', newborder)
+
+
+class TestPurgeResults(TestCase):
+
+    def setUp(self):
+        django.setup()
+        self.source_file_id = 1234321
+        trigger_event = trigger_test_utils.create_trigger_event()
+        PurgeResults.objects.create(source_file_id=self.source_file_id, trigger_event=trigger_event)
+
+    def test_access(self):
+        tmp = PurgeResults.objects.filter(source_file_id=self.source_file_id)
+        self.assertEqual(tmp.count(), 1)
+        self.assertEqual(tmp[0].num_jobs_deleted, 0)
+        self.assertEqual(tmp[0].num_recipes_deleted, 0)
+        self.assertEqual(tmp[0].num_products_deleted, 0)
+
+    def test_not_found(self):
+        tmp = PurgeResults.objects.filter(source_file_id=4321234)
+        self.assertEqual(tmp.count(), 0)
 
 
 class TestWorkspaceManager(TransactionTestCase):
