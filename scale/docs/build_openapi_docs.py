6b538657985002111de350fbeae1850c542027af
Mike Holt <mike.n.holt@gmail.com>
2018-10-11 10:50:06 -0400

    :hammer: updated stiching logic

--- scale/docs/build_openapi_docs.py
+++ scale/docs/build_openapi_docs.py
@@ -5,7 +5,8 @@ import os
 import sys
 import yaml
 
-def build_openapi_docs(root_file, parse_dir, dest_dir):
+
+def build_openapi_docs(root_file, parse_dir):
     """Parses a directory of YAML files for OpenAPI chunks (paths, components, etc.) and injects them into
     the given root file and writes to the desired location.
 
@@ -13,39 +14,58 @@ def build_openapi_docs(root_file, parse_dir, dest_dir):
     - The given root_file MUST be prepopulated with `info` and `server` chunks
     - Only files ending in .yml or .yaml will be parsed
     - The following OpenAPI sections will be detected: paths, components, security, tags, externalDocs
-    
+    - If your root_file is in the parse_dir it will NOT be parsed
+
     inputs:
     - root_file - File path to the openapi.yaml file that parsed OpenAPI chunks will be attached to
     - parse_dir - The directory to parse for OpenAPI chunks
-    - dest_dir - The directory that the resulting openapi.yml will be written to
 
     outputs:
-    - A complete OpenAPI spec written to the desired directory 
+    - A complete OpenAPI spec written to the desired root_file
     """
 
     chunks = {
-        'paths': [],
-        'components': [],
-        'security': [],
-        'tags': [],
-        'externalDocs': []
+        'paths': {},
+        'components': {
+            'schemas': {}
+        },
+        'security': {},
+        'tags': {},
+        'externalDocs': {}
     }
 
-    for file in os.listdir(parse_dir):
-        if file.endswith(".yml") or file.endswith(".yaml"):
-            with open(os.path.join(parse_dir, file), 'r') as f:
+    for _file in os.listdir(parse_dir):
+        if _file == os.path.basename(root_file):
+            continue
+
+        if _file.endswith(".yml") or _file.endswith(".yaml"):
+            with open(os.path.join(parse_dir, _file), 'r') as f:
                 try:
                     yml = yaml.load(f)
                 except yaml.scanner.ScannerError:
-                    print ' '.join(["Error parsing file :", os.path.join(parse_dir, file)])
+                    print ' '.join(["Error parsing file :", os.path.join(parse_dir, _file)])
                 except yaml.parser.ParserError:
-                    print ' '.join(["Error parsing file :", os.path.join(parse_dir, file)])
-                print(type(yml))
+                    print ' '.join(["Error parsing file :", os.path.join(parse_dir, _file)])
 
+                for chunk_type in chunks:
+                    if chunk_type in yml:
+                        c = yml[chunk_type]
+                        for k, v in c.items():
+                            if k == 'schemas':
+                                s = yml[chunk_type][k]
+                                for k, v in s.items():
+                                    chunks[chunk_type]['schemas'][k] = v
+                            else:
+                                chunks[chunk_type][k] = v
+
+    chunks = dict((k, v) for k, v in chunks.iteritems() if v)
+
+    with open(root_file, 'a') as f:
+        f.write(yaml.dump(chunks))
 
 if __name__ == '__main__':
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'f:p:d', ['root_file=', 'parse_dir=', 'dest_dir'])
+        opts, _ = getopt.getopt(sys.argv[1:], 'f:p:', ['root_file=', 'parse_dir='])
     except getopt.GetoptError:
         sys.exit(2)
     for opt, arg in opts:
@@ -53,11 +73,9 @@ if __name__ == '__main__':
             root_file = arg
         elif opt in ('-p', '--parse_dir'):
             parse_dir = arg
-        elif opt in ('-d', '--dest_dir'):
-            dest_dir = arg
         else:
             sys.exit(2)
-    
-    build_openapi_docs(root_file, parse_dir, dest_dir)
+
+    build_openapi_docs(root_file, parse_dir)
 
     sys.exit(0)
