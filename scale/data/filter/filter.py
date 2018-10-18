65a7d8aaae49c3c9db21a1cb5473c30c50f39888
Derick Faller <derick.faller.ctr@us.af.mil>
2018-10-18 14:57:37 +0000

    Created skeleton for data filter class

---
+++ scale/data/filter/filter.py
@@ -0,0 +1,49 @@
+"""Defines the class for filtering data"""
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from data.filter.exceptions import InvalidDataFilter
+
+
+class DataFilter(object):
+    """Represents a filter that either accepts or denies a set of data values
+    """
+
+    def __init__(self, accept):
+        """Constructor
+
+        :param accept: Whether this filter should accept or deny the data
+        :type accept: bool
+        """
+
+        # TODO: take out accept param and do real implementation
+        self.accept = accept
+
+    def is_data_accepted(self, data):
+        """Indicates whether the given data passes the filter or not
+
+        :param data: The data to check against the filter
+        :type data: :class:`data.data.data.Data`
+        :returns: True if the data is accepted, False if the data is denied
+        :rtype: bool
+        """
+
+        # TODO: provide real implementation
+        return self.accept
+
+    def validate(self, interface):
+        """Validates this data filter against the given interface
+
+        :param interface: The interface describing the data that will be passed to the filter
+        :type interface: :class:`data.interface.interface.Interface`
+        :returns: A list of warnings discovered during validation
+        :rtype: list
+
+        :raises :class:`data.filter.exceptions.InvalidDataFilter`: If the data filter is invalid
+        """
+
+        warnings = []
+
+        # TODO: implement
+
+        return warnings
