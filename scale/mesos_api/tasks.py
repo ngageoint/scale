6f5cd27ef4486e37c9792219019f6ab996655e5a
Jonathan Meyer <jon@gisjedi.com>
2018-10-29 13:19:49 -0400

    Updates to JSON format for task creation

--- scale/mesos_api/tasks.py
+++ scale/mesos_api/tasks.py
@@ -8,6 +8,10 @@ from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
+RESOURCE_TYPE_SCALAR = 'SCALAR'
+CONTAINER_TYPE_DOCKER = 'DOCKER'
+NETWORK_TYPE_BRIDGE = 'BRIDGE'
+
 
 def create_mesos_task(task):
     """Creates and returns a Mesos task from a Scale task
@@ -30,24 +34,27 @@ def _create_base_task(task):
     :param task: The task
     :type task: :class:`job.tasks.base_task.Task`
     :returns: The base Mesos task
-    :rtype: :class:`mesos_pb2.TaskInfo`
+    :rtype: dict
     """
 
-    mesos_task = mesos_pb2.TaskInfo()
-    mesos_task.task_id.value = task.id
-    mesos_task.slave_id.value = task.agent_id
-    mesos_task.name = task.name
+    mesos_task = {}
+
+    mesos_task['task_id'] = task.id
+    mesos_task['agent_id'] = task.agent_id
+    mesos_task['name'] = task.name
     resources = task.get_resources()
 
     if settings.CONFIG_URI:
-        mesos_task.command.uris.add().value = settings.CONFIG_URI
+        mesos_task['command'] = { 'uri': [settings.CONFIG_URI] }
 
+    mesos_task['resources'] = task_resources = []
     for resource in resources.resources:
         if resource.value > 0.0:
-            task_resource = mesos_task.resources.add()
-            task_resource.name = resource.name
-            task_resource.type = mesos_pb2.Value.SCALAR
-            task_resource.scalar.value = resource.value
+            task_resource = {}
+            task_resource['name'] = resource.name
+            task_resource['type'] = RESOURCE_TYPE_SCALAR
+            task_resource['scalar'] = { 'value': resource.value }
+            task_resources.append(task_resource)
 
     return mesos_task
 
@@ -58,14 +65,16 @@ def _create_command_task(task):
     :param task: The task
     :type task: :class:`job.tasks.base_task.Task`
     :returns: The command-line Mesos task
-    :rtype: :class:`mesos_pb2.TaskInfo`
+    :rtype: dict
     """
 
     mesos_task = _create_base_task(task)
     command = task.command if task.command else 'echo'
     if task.command_arguments:
         command += ' ' + task.command_arguments
-    mesos_task.command.value = command
+    mesos_task['command'].update(
+        {'value': command}
+    )
 
     return mesos_task
 
@@ -76,25 +85,30 @@ def _create_docker_task(task):
     :param task: The task
     :type task: :class:`job.tasks.base_task.Task`
     returns: The Dockerized Mesos task
-    rtype: :class:`mesos_pb2.TaskInfo`
+    :rtype: dict
     """
 
     mesos_task = _create_base_task(task)
-    mesos_task.container.type = mesos_pb2.ContainerInfo.DOCKER
-    mesos_task.container.docker.image = task.docker_image
-    for param in task.docker_params:
-        mesos_task.container.docker.parameters.add(key=param.flag, value=param.value)
+    mesos_task.update(
+        {
+            'command': {
+                'shell': False,
+                'arguments': [x for x in task.command_arguments.split(" ")]
+            },
+            'container': {
+                'type': CONTAINER_TYPE_DOCKER,
+                'docker': {
+                    'image': task.docker_image,
+                    'parameters': {param.flag: param.value for param in task.docker_params},
+                    'network': NETWORK_TYPE_BRIDGE,
+                    'force_pull_image': False
+                },
+            }
+        }
+    )
+
+    # Remove this once docker privileged is removed.
     if task.is_docker_privileged:
-        mesos_task.container.docker.privileged = True
-
-    # Use Docker image entrypoint
-    mesos_task.command.shell = False
-
-    arguments = task.command_arguments.split(" ")
-    for argument in arguments:
-        mesos_task.command.arguments.append(argument)
-
-    mesos_task.container.docker.network = mesos_pb2.ContainerInfo.DockerInfo.Network.Value('BRIDGE')
-    mesos_task.container.docker.force_pull_image = False
+        mesos_task['container']['docker']['privileged'] = True
 
     return mesos_task
