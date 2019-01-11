0c42ba97eebcd8f4a77e036a4804e605a53e595f
emimaesmith <emimaesmith@gmail.com>
2019-01-11 16:29:14 +0000

    :white_check_mark: Adding tests #1067

--- scale/dataset/test/test_models.py
+++ scale/dataset/test/test_models.py
@@ -10,32 +10,84 @@ import django
 import django.utils.timezone as timezone
 from django.test import TestCase, TransactionTestCase
 
+from dataset.models import DataSet
+import dataset.test.utils as dataset_test_utils
+from dataset.definition.json.definition_v6 import DataSetDefinition
+
 class TestDataSetManager(TransactionTestCase):
 
     def setUp(self):
         django.setup()
 
+        definition = {}
+        self.dataset_definition = DataSetDefinition(definition)
+
+    def test_create_dataset(self):
+        """Tests calling DataSet.create() """
+
+        name = 'test-dataset'
+        title = 'Test Dataset'
+        description = 'Test DataSet description'
+        version = '1.0.0'
+        definition = {}
+
+        # call test
+        dataset = dataset_test_utils.create_dataset(name=name, title=title, description=description,
+            version=version, definition=definition)
+
+        # Check results
+        the_dataset = DataSet.objects.get(pk=dataset.id)
+        self.assertEqual(the_dataset.name, name)
+        self.assertEqual(the_dataset.title, title)
+        self.assertEqual(the_dataset.version, version)
+        self.assertDictEqual(the_dataset.definition, definition)
+
     def test_create_dataset_v6(self):
         """Tests calling DataSetManager.create_dataset_v6() """
 
+        name = 'test-dataset'
+        title = 'Test Dataset'
+        description = 'Test DataSet description'
+        version = '1.0.0'
+        definition = {}
+
+        # call test
+        dataset = DataSet.objects.create_dataset_v6(version, definition, name=name, title=title, description=description)
+
+        # Check results
+        the_dataset = DataSet.objects.get(pk=dataset.id)
+        self.assertEqual(the_dataset.name, name)
+        self.assertEqual(the_dataset.title, title)
+        self.assertEqual(the_dataset.version, version)
+        self.assertDictEqual(the_dataset.definition, definition)
+
     def test_filter_datasets(self):
-        """Tests calling DataSetManager.filterdatasets """
+        """Tests calling DataSetManager filter_datasets
+        """
+
+        pass
 
     def test_filter_datasets_related_v6(self):
         """Tests calling DataSetManager.filter_datasets_related() """
-        
+
+        pass
+
     def test_get_datasets_v6(self):
         """Tests calling DataSetmanager.get_datasets_v6() """
-        
+
+        pass
+
     def test_get_dataset_details(self):
         """Tests calling DataSetManager.get_dataset_details() """
-        
-class TestDataSet(TransactionTestCase):
 
-    def setUp(self):
-        django.setup()
-        
-        dataset_def_str = \
-            """
-            """
-        
+        pass
+
+#class TestDataSet(TransactionTestCase):
+
+#    def setUp(self):
+#        django.setup()
+
+        # dataset_def_str = \
+        #     """
+        #     """
+
--- scale/dataset/test/test_views.py
+++ scale/dataset/test/test_views.py
@@ -13,6 +13,7 @@ from mock import patch
 from rest_framework import status
 
 from dataset.models import DataSet
+import dataset.test.utils as dataset_test_utils
 
 """Tests the v6/datasets/ endpoint"""
 class TestDatasetViews(TestCase):
@@ -21,10 +22,10 @@ class TestDatasetViews(TestCase):
     def setUp(self):
         django.setup()
 
-        self.date_1 = datetime.datetime(2016, 1, 1, tzinfo=utc)
-
-        #self.datset = dataset_test_utils.create_dataset(name='test-dataset-1', version='1.0.0')
-        self.dataset = None
+        self.dataset = dataset_test_utils.create_dataset(name='test-dataset-1',
+            title="Test Dataset 1", description="Test Dataset Number 1", version='1.0.0')
+        self.dataset2 = dataset_test_utils.create_dataset(name='test-dataset-2',
+            title="Test Dataset 2", description="Test Dataset Number 2", version='1.0.0')
 
     def test_successful(self):
         """Tests successfully calling the v6/datasets/ view.
@@ -34,33 +35,46 @@ class TestDatasetViews(TestCase):
         response = self.client.generic('GET', url)
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
-        # Test response contains specific dataset 
+        # Test response contains specific dataset
+        result = json.loads(response.content)
+        self.assertEqual(len(result['results']), 2)
+        for entry in result['results']:
+            expected = None
+            if entry['id'] == self.dataset.id:
+                expected = self.dataset
+            elif entry['id'] == self.dataset2.id:
+                expected = self.dataset2
+            else:
+                self.fail('Found unexpected result: %s' % entry['id'])
+            self.assertEqual(entry['name'], expected.name)
+            self.assertEqual(entry['version'], expected.version)
+            self.assertEqual(entry['title'], expected.title)
 
-    def test_dataset_created_time_successfull(self):
+    def test_dataset_created_time_successful(self):
         """Tests successfully calling the v6/datsets/?created_time= api call
         """
 
-        url = '/%s/datasets/?created_time=%s' % (self.api, self.date_1)
+        url = '/%s/datasets/?created=%s' % (self.api, '2016-01-01T00:00:00Z')
         response = self.client.generic('GET', url)
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
         # Verify one result
 
-    def test_dataset_dataset_id_successfull(self):
-        """Tests successfully calling the v6/datsets/?dataset_id= api call
+    def test_dataset_id_successful(self):
+        """Tests successfully calling the v6/datsets/?id= api call
         """
 
-        url = '/%s/datasets/?dataset_id=%s' % (self.api, self.dataset.id)
+        url = '/%s/datasets/?id=%s' % (self.api, self.dataset.id)
         response = self.client.generic('GET', url)
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
         # Verify one result
 
-    def test_dataset_dataset_name_successfull(self):
-        """Tests successfully calling the v6/datsets/?dataset_name= api call
+    def test_dataset_name_successful(self):
+        """Tests successfully calling the v6/datsets/?name= api call
         """
 
-        url = '/%s/datasets/?dataset_name=%s' % (self.api, self.dataset.name)
+        url = '/%s/datasets/?name=%s' % (self.api, self.dataset.name)
         response = self.client.generic('GET', url)
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
@@ -69,96 +83,203 @@ class TestDatasetViews(TestCase):
     def test_order_by(self):
         """Tests successfully calling the datasets view with sorting."""
 
-        url = '/%s/datasets/?order=dataset__name' % self.api
+        url = '/%s/datasets/?order=name' % self.api
         response = self.client.generic('GET', url)
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
-        # Verify X results
+        # Verify 2 results
         result = json.loads(response.content)
+        self.assertEqual(len(result['results']), 2)
 
 """Tests the v6/datasets POST calls """
-class TestDataSetsPostView(TestCase):
+class TestDataSetPostView(TestCase):
     """Tests the v6/dataset/ POST API call"""
     api = 'v6'
-    
+
     def setUp(self):
         django.setup()
-        
+
     def test_invalid_definition(self):
         """Tests successfully calling POST with an invalid definition."""
         json_data = {}
-        
+
         url = '/%s/datasets/' % self.api
         response = self.client.generic('POST', url, json.dumps(json_data), 'application/json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.content)
 
     def test_add_dataset(self):
         """Tests adding a new dataset"""
-        
-        url = '/%s/datasets' % self.api
-        
+
+        url = '/%s/datasets/' % self.api
+
         json_data = {
             'name': 'my-new-dataset',
             'version': '1.0.0',
             'title': 'My Dataset',
             'description': 'A test dataset',
             'definition': {
-                
             },
         }
 
         response = self.client.generic('POST', url, json.dumps(json_data), 'application/json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.content)
         self.assertTrue('/%s/datasets/my-new-dataset/1.0.0/' % self.api in response['location'])
-        
+
         dataset = DataSet.objects.filter(name='my-new-dataset').first()
-        
-        # check the dataset created
-        results = json.loads(response.content)
-        # self.assertEqual(results['id'], dataset.id)
-        # self.assertEqual(results['version'], dataset.version)
-        # self.assertEqual(results['title'], dataset.title)
-        # self.assertEqual(results['revision_num'], dataset.revision_num)
-        
-"""Tests the v6/datasets/<dataset_id> and v6/datsets/<dataset_name> endpoints"""  
+        self.assertEqual(dataset.name, json_data['name'])
+        self.assertEqual(dataset.title, json_data['title'])
+        self.assertEqual(dataset.version, json_data['version'])
+
+        # update the dataset
+        json_data_2 = {
+            'name': 'my-new-dataset',
+            'version': '1.0.0',
+            'title': 'My Dataset',
+            'description': 'An updated test dataset',
+            'definition': {
+            },
+        }
+        response = self.client.generic('POST', url, json.dumps(json_data_2), 'application/json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.content)
+        self.assertTrue('/%s/datasets/my-new-dataset/1.0.0/' % self.api in response['location'])
+
+        # check the dataset was updated
+        dataset = DataSet.objects.filter(name='my-new-dataset').first()
+        self.assertEqual(dataset.description, json_data_2['description'])
+
+
+"""Tests the v6/datasets/<dataset_id> and v6/datsets/<dataset_name> endpoints"""
 class TestDatasetDetailsView(TestCase):
     api = 'v6'
 
     def setUp(self):
         django.setup()
-        
-        self.dataset = None
-        self.empty_dataset = None
-        # Create dataset with basic values
-        
-    def test_successful_empty(self):
-        """Tests successfully calling the dataset details view with no data or results"""
 
-        url = '/%s/datasets/%s/' % (self.api, self.empty_dataset.name)
-        response = self.client.generic('GET', url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
-        
+        # Create dataset with basic values
+        self.dataset = dataset_test_utils.create_dataset(name='test-dataset-1',
+            title="Test Dataset 1", description="Test Dataset Number 1", version='1.0.0')
+        self.dataset2 = dataset_test_utils.create_dataset(name='test-dataset-1',
+            title="Test Dataset 1", description="Updated Test Dataset Number 1", version='1.1.1')
+
+    # def test_successful_empty(self):
+    #     """Tests successfully calling the dataset details view with no data or results"""
+
+    #     url = '/%s/datasets/%s/' % (self.api, 'empty-dataset')
+    #     response = self.client.generic('GET', url)
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
+
         # check response for stuff
-        result = json.loads(response.content)
+        # result = json.loads(response.content)
         # self.assertEqual(response[''], self.empty_dataset.)
 
     def test_datasets_id_successful(self):
         """Tests successfully calling the v6/datasets/<dataset_id>/ view.
         """
 
-        url = '/%s/datasets/%d' % (self.api, self.dataset.id)
+        url = '/%s/datasets/%d/' % (self.api, self.dataset.id)
         response = self.client.generic('GET', url)
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
 
         # Check response for dataset details
+        result = json.loads(response.content)
+        self.assertEqual(result['id'], self.dataset.id)
+        self.assertEqual(result['name'], self.dataset.name)
+        self.assertEqual(result['title'], self.dataset.title)
+        self.assertEqual(result['description'], self.dataset.description)
+        self.assertEqual(result['version'], self.dataset.version)
+        self.assertDictEqual(result['definition'], self.dataset.definition)
 
-    def test_datasets_name_successfull(self):
+    def test_datasets_name_successful(self):
         """Tests successfully calling the v6/datsets/<dataset-name>/ view.
         """
 
-        url = '/%s/datasets/%d' % (self.api, self.dataset.name)
+        url = '/%s/datasets/%s/' % (self.api, self.dataset.name)
         response = self.client.generic('GET', url)
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
-        
+
         # Check response for dataset details
-        
+        results = json.loads(response.content)
+        self.assertEqual(len(results['results']), 2)
+        for entry in results['results']:
+            expected = None
+            if entry['id'] == self.dataset.id:
+                expected = self.dataset
+            elif entry['id'] == self.dataset2.id:
+                expected = self.dataset2
+            else:
+                self.fail('Found unexpected result: %s' % entry['id'])
+
+            self.assertEqual(entry['name'], expected.name)
+            self.assertEqual(entry['title'], expected.title)
+            self.assertEqual(entry['version'], expected.version)
+            # self.assertEqual(entry['versions'], ['1.0.0', '1.1.1'])
+
+    def test_datasets_name_version_successful(self):
+        """Tests successfully calling the v6/datasets/<dataset-name>/<version> view.
+        """
+
+        url = '/%s/datasets/%s/%s/' % (self.api, self.dataset.name, self.dataset.version)
+        response = self.client.generic('GET', url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
+
+        # check response for details
+        result = json.loads(response.content)
+        self.assertEqual(result['title'], self.dataset.title)
+        self.assertEqual(result['description'], self.dataset.description)
+
+        url = '/%s/datasets/%s/%s/' % (self.api, self.dataset2.name, self.dataset2.version)
+        response = self.client.generic('GET', url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
+
+        # check response for details
+        result = json.loads(response.content)
+        self.assertEqual(result['title'], self.dataset2.title)
+        self.assertEqual(result['description'], self.dataset2.description)
+
+
+class TestDataSetValidationView(TestCase):
+    api = 'v6'
+
+    def setUp(self):
+        django.setup()
+
+    def test_validate_successful(self):
+        url = '/%s/datasets/validation/' % self.api
+
+        json_data = {
+            'name': 'test-dataset',
+            'title': 'Test Dataset',
+            'description': 'My Test Dataset',
+            'version': '1.0.0',
+            'definition': {}
+        }
+        response = self.client.generic('POST', url, json.dumps(json_data), 'application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
+
+        results = json.loads(response.content)
+        self.assertTrue(results['is_valid'])
+        self.assertEqual(len(results['warnings']), 0)
+        self.assertEqual(len(results['errors']), 0)
+
+    def test_validate_missing_definition(self):
+        url = '/%s/datasets/validation/' % self.api
+
+        json_data = {
+            'name': 'test-dataset',
+            'title': 'Test Dataset',
+            'description': 'My Test Dataset',
+            'version': '1.0.0',
+        }
+
+        response = self.client.generic('POST', url, json.dumps(json_data), 'application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.content)
+
+        results = json.loads(response.content)
+        self.assertEqual(results['detail'], "Missing required parameter: \"definition\"")
+
+    def test_validate_invalid_definition(self):
+        """Validates an invalid dataset definition
+
+        Will complete when dataset definition is defined.
+        """
+        pass
