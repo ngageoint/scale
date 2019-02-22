b838c0ec095021944ea49ddb73bf663e6a241de8
emimaesmith <emimaesmith@gmail.com>
2019-02-22 14:34:10 +0000

    :fire: Removing v5 error endpoints

--- scale/error/serializers.py
+++ scale/error/serializers.py
@@ -3,26 +3,6 @@ import rest_framework.serializers as serializers
 
 from util.rest import ModelIdSerializer
 
-
-class ErrorBaseSerializerV5(ModelIdSerializer):
-    """Converts error model fields to REST output"""
-    name = serializers.CharField()
-    title = serializers.CharField()
-    description = serializers.CharField()
-    category = serializers.CharField()
-    is_builtin = serializers.BooleanField()
-
-
-class ErrorSerializerV5(ErrorBaseSerializerV5):
-    """Converts error model fields to REST output"""
-    created = serializers.DateTimeField()
-    last_modified = serializers.DateTimeField()
-
-
-class ErrorDetailsSerializerV5(ErrorSerializerV5):
-    """Converts error model fields to REST output"""
-    pass
-
 class ErrorBaseSerializerV6(ModelIdSerializer):
     """Converts error model fields to REST output"""
     name = serializers.CharField()
