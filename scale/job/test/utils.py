0ea047e6b0b4de77860b3b982daf0cd7571855b4
emimaesmith <emimaesmith@gmail.com>
2019-02-25 20:31:57 +0000

    :fire::construction: updated tests to refleact model changes

--- scale/job/test/utils.py
+++ scale/job/test/utils.py
@@ -235,7 +235,9 @@ def create_job(job_type=None, event=None, status='PENDING', error=None, input=No
     recipe_id = recipe.id if recipe else None
     root_recipe_id = recipe.root_superseded_recipe_id if recipe else None
 
-    job_type_rev = JobTypeRevision.objects.get_revision(job_type.name, job_type.version, job_type.revision_num)
+    # TODO: fix this - Mike
+    revision_num = 0
+    job_type_rev = JobTypeRevision.objects.get_revision(job_type.name, job_type.version, revision_num)
     job = Job.objects.create_job_v6(job_type_rev, event_id=event.id, superseded_job=superseded_job, recipe_id=recipe_id,
                                     root_recipe_id=root_recipe_id)
     job.priority = priority
@@ -255,7 +257,6 @@ def create_job(job_type=None, event=None, status='PENDING', error=None, input=No
     if save:
         job.save()
     return job
-
 
 def create_job_exe(job_type=None, job=None, exe_num=None, node=None, timeout=None, input_file_size=10.0, queued=None,
                    started=None, status='RUNNING', error=None, ended=None, output=None, task_results=None, configuration=None):
@@ -491,6 +492,7 @@ def create_seed_job_type(manifest=None, priority=50, max_tries=3, max_scheduled=
     JobTypeRevision.objects.create_job_type_revision(job_type)
     return job_type
 
+
 def edit_job_type_v6(job_type, manifest_dict=None, docker_image=None, icon_code=None, is_active=None,
                      is_published=None, is_paused=None, max_scheduled=None, configuration_dict=None):
     """Updates a job type, including creating a new revision for unit testing
@@ -505,7 +507,6 @@ def edit_job_type_v6(job_type, manifest_dict=None, docker_image=None, icon_code=
     JobType.objects.edit_job_type_v6(job_type.id, manifest=manifest, docker_image=docker_image,
                          icon_code=icon_code, is_active=is_active, is_paused=is_paused,
                          is_published=is_published, max_scheduled=max_scheduled, configuration=configuration)
-
 
 def create_running_job_exe(agent_id='agent_1', job_type=None, job=None, node=None, timeout=None, input_file_size=10.0,
                            queued=None, started=None, resources=None, priority=None, num_exes=1):
