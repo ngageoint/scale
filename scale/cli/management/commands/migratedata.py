2991490b1c45b13eec06367fc788910a951fd6aa
emimaesmith <emimaesmith@gmail.com>
2019-02-26 20:48:00 +0000

    :fire: Removing ingest v5 APIs; fixed the scale system job seed declarations

--- scale/cli/management/commands/migratedata.py
+++ scale/cli/management/commands/migratedata.py
@@ -120,6 +120,7 @@ class Command(BaseCommand):
                     if options['recipe']:
                         IngestRecipeHandler().process_ingested_source_file(ingest.id, ingest.source_file, ingest.ingest_ended, options['recipe'])
 
+
         logging.info("Ingests processed, monitor the queue for triggered jobs.")
 
         if mnt_dirs is not None:
