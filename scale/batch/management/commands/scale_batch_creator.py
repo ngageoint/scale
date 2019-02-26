12467ef282a58725433a4ab4cd560a60c672ee5a
emimaesmith <emimaesmith@gmail.com>
2019-02-26 18:14:30 +0000

    :fire: Removing batch v5 apis

--- scale/batch/management/commands/scale_batch_creator.py
+++ scale/batch/management/commands/scale_batch_creator.py
@@ -8,6 +8,8 @@ from optparse import make_option
 from django.core.management.base import BaseCommand
 
 from batch.models import Batch
+from batch.messages.create_batch_recipes import create_batch_recipes_message
+from messaging.manager import CommandMessageManager
 
 logger = logging.getLogger(__name__)
 
@@ -16,7 +18,7 @@ class Command(BaseCommand):
     """Command that creates a Scale batch"""
 
     help = 'Creates a new batch of jobs and recipes to be processed on the cluster'
-    
+
     def add_arguments(self, parser):
         parser.add_argument('-i', '--batch-id', action='store', type=int,
                             help='The ID of the batch to create')
@@ -33,7 +35,8 @@ class Command(BaseCommand):
 
         # Schedule all the batch recipes
         try:
-            Batch.objects.schedule_recipes(batch_id)
+            batch = Batch.objects.get(id=batch_id)
+            CommandMessageManager().send_messages([create_batch_recipes_message(batch_id)])
         except Batch.DoesNotExist:
             logger.exception('Unable to find batch: %i', batch_id)
             sys.exit(1)
