40452676700f3133c63d6cde047dc3fef0c0f5e4
Jonathan Meyer <jon@gisjedi.com>
2019-02-28 15:20:42 -0500

    Added additional persistence services to purge

--- dev-build-push.sh
+++ dev-build-push.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 dcos marathon app remove /scale-db
+dcos marathon app remove /scale-elasticsearch
 dcos marathon app remove /scale-fluentd
 dcos marathon app remove /scale-webserver
+dcos marathon app remove /scale-rabbitmq
 dcos marathon app stop /scale
 
 docker build -t $1 -f Dockerfile-dev .
