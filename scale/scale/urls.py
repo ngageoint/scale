aef3c5d0a590bc9ee71099455abf2578dc3cce9e
Jonathan Meyer <jon@gisjedi.com>
2019-03-04 16:14:39 -0500

    Updated custom login view to override all defaults

--- scale/scale/urls.py
+++ scale/scale/urls.py
@@ -1,11 +1,11 @@
 """Combines all of the URLs for the Scale RESTful services"""
 
 import util.rest as rest_util
-
 from django.conf import settings
 from django.conf.urls import include, url
-from rest_framework.authtoken import views
 from django.contrib.auth import views as auth_views
+from rest_framework.authtoken import views
+
 
 
 # Enable the admin applications
@@ -30,16 +30,24 @@ REST_API_APPS = [
 
 # Generate URLs for all REST APIs with version prefix
 urlpatterns = rest_util.get_versioned_urls(REST_API_APPS)
-
+login_settings = \
+    {
+        'template_name': 'accounts/login.html',
+        'extra_context':
+            {
+                'GEOAXIS_ENABLED': settings.GEOAXIS_ENABLED
+            }
+    }
 unversioned_urls = [
-    # Map all the paths required by the admin applications
+    # Map all the various login endpoints to share our custom auth page
+    # Our custom login view must appear FIRST to override subsequent login views
+    url(r'^admin/login/', auth_views.login, login_settings),
+    url(r'^api-auth/login/',auth_views.login, login_settings),
+    url(r'^login/$', auth_views.login, login_settings),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth/', views.obtain_auth_token),
     url(r'^social-auth/', include('social_django.urls', namespace='social')),
-    url(r'^login/$', auth_views.login,
-        {'template_name': 'accounts/login.html', 'extra_context':{'GEOAXIS_ENABLED': settings.GEOAXIS_ENABLED}},
-        name='login'),
 ]
 
 # Add unversioned_urls to URL regex pattern matcher
