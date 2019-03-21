61435267c82d25e469eac43b203319e87980953b
emimaesmith <emimaesmith@gmail.com>
2019-03-21 11:24:03 +0000

    :tada: batch tests pass

--- scale/batch/test/management/commands/test_scale_batch_creator.py
+++ scale/batch/test/management/commands/test_scale_batch_creator.py
@@ -6,6 +6,8 @@ from mock import patch
 
 from batch.management.commands.scale_batch_creator import Command as BatchCommand
 from batch.test import utils as batch_test_utils
+from messaging.backends.amqp import AMQPMessagingBackend
+from messaging.backends.factory import add_message_backend
 from recipe.test import utils as recipe_test_utils
 
 
@@ -16,6 +18,10 @@ class TestBatchCreator(TransactionTestCase):
     def setUp(self):
         django.setup()
 
+        add_message_backend(AMQPMessagingBackend)
+
+        # mock out threading.start
+
         self.recipe_type = recipe_test_utils.create_recipe_type_v6()
         self.batch = batch_test_utils.create_batch(recipe_type=self.recipe_type)
 
@@ -25,8 +31,9 @@ class TestBatchCreator(TransactionTestCase):
         cmd = BatchCommand()
         self.assertRaises(SystemExit, cmd.run_from_argv, ['manage.py', 'scale_batch_creator', '-i', '123'])
 
+    @patch('batch.views.CommandMessageManager')
     @patch('batch.management.commands.scale_batch_creator.Batch.objects')
-    def test_successful(self, mock_batch_manager):
+    def test_successful(self, mock_msg_mgr, mock_batch_manager):
         """Tests calling command for a valid batch"""
 
         cmd = BatchCommand()
