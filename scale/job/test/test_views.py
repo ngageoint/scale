cddfd4b6ec0cd3e21a014e1dac6d3e2e4df0c883
John Tobe <jtobe@ball.com>
2019-04-04 22:11:28 -0400

    :hammer: Fake validation for v5 job types in v6 recipes

--- scale/job/test/test_views.py
+++ scale/job/test/test_views.py
@@ -2267,7 +2267,7 @@ class TestJobTypesPostViewV6(TestCase):
     def test_add_seed_job_type_existing_error(self):
         """Tests adding a seed image with an existing error."""
 
-        existing_error = error_test_utils.create_error(job_type_name='my-new-job')
+        existing_error = error_test_utils.create_error(name='error-name-one', job_type_name='other-job')
         
         url = '/%s/job-types/' % self.api
         manifest = copy.deepcopy(job_test_utils.COMPLETE_MANIFEST)
@@ -2718,6 +2718,32 @@ class TestJobTypeDetailsViewV5(TestCase):
 
         self.error1 = error_test_utils.create_error()
         self.error2 = error_test_utils.create_error()
+
+        definition = {
+            'version': '1.0',
+            'input_data': [{
+                'name': 'Recipe Input',
+                'type': 'file',
+                'media_types': ['text/plain'],
+            }],
+            'jobs': [{
+                'name': 'Job 1',
+                'job_type': {
+                    'name': self.job_type.name,
+                    'version': self.job_type.version,
+                },
+                'recipe_inputs': [{
+                    'recipe_input': 'Recipe Input',
+                    'job_input': 'input_files',
+                }]
+            }]
+        }
+        self.old_recipe_type = recipe_test_utils.create_recipe_type_v5(definition=definition)
+        v6_definition = copy.deepcopy(recipe_test_utils.SUB_RECIPE_DEFINITION)
+        v6_definition['nodes']['node_a']['node_type']['job_type_name'] = self.job_type.name
+        v6_definition['nodes']['node_a']['node_type']['job_type_version'] = self.job_type.version
+        v6_definition['nodes']['node_a']['node_type']['job_type_revision'] = self.job_type.revision_num
+        self.new_recipe_type = recipe_test_utils.create_recipe_type_v6()
 
     def test_not_found(self):
         """Tests successfully calling the get job type details view with a job id that does not exist."""
