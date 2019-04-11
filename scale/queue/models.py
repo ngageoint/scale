cec2964965dfa0f2b4843c882bd8668340c533e1
John Tobe <jtobe@ball.com>
2019-04-11 16:32:05 +0000

    :bug: Fixing missing attribute bug

--- scale/queue/models.py
+++ scale/queue/models.py
@@ -327,7 +327,7 @@ class QueueManager(models.Manager):
             elif job.priority:
                 queued_priority = job.priority
             elif job.batch and self.batch.get_configuration().priority:
-                queued_priority = self.batch.get_configuration().priority
+                queued_priority = self.job.batch.get_configuration().priority
             else:
                 queued_priority = job.get_job_configuration().priority
 
