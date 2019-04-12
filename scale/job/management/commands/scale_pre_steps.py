45281b1caf2c3873b59e17465525cde750733ec1
John Tobe <jtobe@ball.com>
2019-04-12 16:08:42 -0400

    :hammer: Removing unneccessary version from path and actually call the new generate input metadata method.

--- scale/job/management/commands/scale_pre_steps.py
+++ scale/job/management/commands/scale_pre_steps.py
@@ -54,6 +54,8 @@ class Command(BaseCommand):
             logger.info('Validating outputs and workspaces...')
             job_interface.validate_workspace_for_outputs(exe_config)
 
+            self._generate_input_metadata(job_exe)
+
             job_data = job_exe.job.get_job_data()
             job_data = JobDataSunset.create(job_interface, job_data.get_dict())
             logger.info('Setting up input files...')
@@ -90,7 +92,6 @@ class Command(BaseCommand):
 
         return JobExecution.objects.get_job_exe_with_job_and_job_type(job_id, exe_num)
 
-    @staticmethod
     def _generate_input_metadata(self, job_exe):
         """Generate the input metadata file for the job execution
 
@@ -171,8 +172,7 @@ class Command(BaseCommand):
         if job_exe.job.recipe:
             recipe = job_exe.job.recipe
             recipe_type_path = get_valid_filename(recipe.recipe_type.name)
-            recipe_version_path = get_valid_filename(recipe.recipe_type.version)
-            remote_path = os.path.join(remote_path, 'recipes', recipe_type_path, recipe_version_path)
+            remote_path = os.path.join(remote_path, 'recipes', recipe_type_path)
         job_type_path = get_valid_filename(job_exe.job.job_type.name)
         job_version_path = get_valid_filename(job_exe.job.job_type.version)
         remote_path = os.path.join(remote_path, 'jobs', job_type_path, job_version_path)
