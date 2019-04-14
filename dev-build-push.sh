f7ff5eb7d1b6c8abee3a23b7c8d9b2551ff6427f
Jonathan Meyer <jon@gisjedi.com>
2019-04-14 15:30:51 -0400

    Removed remaining scale-ui references

--- dev-build-push.sh
+++ dev-build-push.sh
@@ -5,15 +5,6 @@ dcos marathon app remove /scale-fluentd
 dcos marathon app remove /scale-webserver
 dcos marathon app stop /scale
 
-# Assumes an adjacent scale-ui source code checkout to grab UI assets from
-if [ ! -f ./scale-ui/index.html ]
-then
-    cd ../scale-ui
-    npm run builddev:prod 
-    cd -
-    cp -R ../scale-ui/dist/developer ./scale-ui
-fi
-
 docker build -t $1 -f Dockerfile-dev .
 docker push $1
 
