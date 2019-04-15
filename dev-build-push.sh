8ba68e65fdbcbf4a4155eab1367759a48bf1ca29
Jonathan Meyer <jon@gisjedi.com>
2019-04-15 13:03:08 -0400

    Removed unneeded dev Dockerfile now that UI is split out

--- dev-build-push.sh
+++ dev-build-push.sh
@@ -7,7 +7,7 @@ dcos marathon app remove /scale-webserver
 dcos marathon app remove /scale-rabbitmq
 dcos marathon app stop /scale
 
-docker build -t $1 -f Dockerfile-dev .
+docker build -t $1 .
 docker push $1
 
 dcos marathon app start /scale 1
