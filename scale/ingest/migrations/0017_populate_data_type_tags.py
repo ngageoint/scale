830f4db6b6358720354ca1cb66cfea83fc48620e
John Tobe <jtobe@ball.com>
2019-05-09 19:17:22 +0000

    :hammer: Using raw queries to perform updates in the database for performance

--- scale/ingest/migrations/0017_populate_data_type_tags.py
+++ scale/ingest/migrations/0017_populate_data_type_tags.py
@@ -1,33 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations
+from django.db import connection, migrations
 
 def populate_data_type_tags(apps, schema_editor):
-    # Go through all of the ScaleFile models and convert the data_type string into an array of tags
-    Ingest = apps.get_model('ingest', 'Ingest')
-
-    total_count = Ingest.objects.exclude(data_type=None).count()
-    if not total_count:
-        return
-
-    print('\nCreating new data type tags: %i' % total_count)
-    ingests = Ingest.objects.exclude(data_type=None).iterator()
-    done_count = 0
-    for i in ingests:
-        save_data_type_tags(i)
-
-        done_count += 1
-        percent = (float(done_count) / float(total_count)) * 100.00
-        print('Progress: %i/%i (%.2f%%)' % (done_count, total_count, percent))
+    # Go through all of the Ingest models and convert the data_type string into an array of tags
+    
+    update = 'UPDATE ingest SET data_type_tags = string_to_array(data_type,",") WHERE data_type <> \'\''
+    with connection.cursor() as cursor:
+        cursor.execute(update)
+        count = cursor.rowcount
+        if count:
+            print('%d entries updated with data type tags' % count)
 
     print ('Migration finished.')
-
-def save_data_type_tags(ingest):
-        tags = set()
-        if ingest.data_type:
-            ingest.data_type_tags = ingest.data_type.split(',')
-            ingest.save()
 
 class Migration(migrations.Migration):
 
