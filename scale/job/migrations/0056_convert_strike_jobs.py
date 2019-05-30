aba412f861af5b65d9275e1292791f4873515e74
EC2 Default User <ec2-user@ip-172-31-11-199.us-east-2.compute.internal>
2019-05-30 16:43:37 +0000

    #1650 Adding migration to update strike jobs Strike ID key

---
+++ scale/job/migrations/0056_convert_strike_jobs.py
@@ -0,0 +1,37 @@
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+def convert_strike_job_inputs(apps, schema_editor):
+    Strike = apps.get_model('ingest', 'Strike')
+    Job = apps.get_model('job', 'Job')
+    
+    strikes = Strike.objects.all().iterator()
+    for strike in strikes:
+        strike_job = Job.objects.get(pk=strike.job_id)
+        
+        # v5 job data
+        changed = False
+        if strike_job.input['version'] == '6':
+            if 'Strike ID' in strike_job.input['json']:
+                strike_job.input['json']['Strike_ID'] = strike_job.input['json']['Strike ID']
+                del strike_job.input['json'][k]
+                changed = True
+        else:
+            for i_data in strike_job.input['input_data']:
+                if i_data['name'] == 'Strike ID':
+                    i_data['name'] = 'Strike_ID'
+                    changed = True
+
+        if changed:
+            strike_job.save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('job', '0055_jobtype_v5_deprecation'),
+    ]
+
+    operations = [
+       migrations.RunPython(convert_strike_job_inputs),
+    ]
