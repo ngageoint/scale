905dec87b9bae506628b9a3706017885325fc0fe
John Tobe <jtobe@ball.com>
2019-06-04 16:32:04 +0000

    :twisted_rightwards_arrows: Merging migrations and making them inherit from new migration pulled in from master

--- scale/job/migrations/0057_auto_20190603_1846.py
+++ scale/job/migrations/0057_auto_20190603_1846.py
@@ -8,10 +8,106 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('job', '0056_auto_20190603_1535'),
+        ('job', '0056_convert_strike_jobs'),
     ]
 
     operations = [
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='command_arguments',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='cpus_scheduled',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='disk_out_scheduled',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='disk_total_scheduled',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='ended',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='environment',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='error',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='job_completed',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='job_exit_code',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='job_metrics',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='job_started',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='last_modified',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='mem_scheduled',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='post_completed',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='post_exit_code',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='post_started',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='pre_completed',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='pre_exit_code',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='pre_started',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='results',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='results_manifest',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='status',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='stderr',
+        ),
+        migrations.RemoveField(
+            model_name='jobexecution',
+            name='stdout',
+        ),
         migrations.AlterUniqueTogether(
             name='jobexecution',
             unique_together=set([('job', 'exe_num')]),
