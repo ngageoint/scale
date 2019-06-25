d4dc838bd46de29e632bfe51270bdf11ec75cc47
EC2 Default User <ec2-user@ip-172-31-11-199.us-east-2.compute.internal>
2019-06-25 15:56:58 +0000

    what if we remove message backend

--- scale/ingest/test/messages/test_create_ingest_jobs.py
+++ scale/ingest/test/messages/test_create_ingest_jobs.py
@@ -11,12 +11,11 @@ import recipe.test.utils as recipe_test_utils
 import storage.test.utils as storage_test_utils
 from job.models import Job
 from ingest.messages.create_ingest_jobs import create_strike_ingest_job_message, \
-                                               create_scan_ingest_job_message, CreateIngest, STRIKE_JOB_TYPE
+                                               create_scan_ingest_job_message,  \
+                                               CreateIngest, STRIKE_JOB_TYPE
 from ingest.models import Ingest, Strike, Scan
 from ingest.scan.configuration.json.configuration_v6 import ScanConfigurationV6
 from ingest.strike.configuration.json.configuration_v6 import StrikeConfigurationV6
-from messaging.backends.amqp import AMQPMessagingBackend
-from messaging.backends.factory import add_message_backend
 from storage.models import ScaleFile
 
 
@@ -26,26 +25,29 @@ class TestCreateIngest(TestCase):
     
     def setUp(self):
         django.setup()
-        add_message_backend(AMQPMessagingBackend)
-
-        self.workspace_1 = storage_test_utils.create_workspace()
-        self.workspace_2 = storage_test_utils.create_workspace()
-        self.source_file = ScaleFile.objects.create(file_name='input_file', file_type='SOURCE',
+        
+        
+    def test_json_create(self):
+        """Tests converting a CreateIngest message to and from json
+        """
+        workspace_1 = storage_test_utils.create_workspace()
+        workspace_2 = storage_test_utils.create_workspace()
+        source_file = ScaleFile.objects.create(file_name='input_file', file_type='SOURCE',
                                                media_type='text/plain', file_size=10, data_type_tags=['type1'],
-                                               file_path='the_path', workspace=self.workspace_1)
+                                               file_path='the_path', workspace=workspace_1)
 
-        self.source_file.add_data_type_tag('type1')
-        self.source_file.add_data_type_tag('type2')
-        self.source_file.add_data_type_tag('type3')
+        source_file.add_data_type_tag('type1')
+        source_file.add_data_type_tag('type2')
+        source_file.add_data_type_tag('type3')
 
-        self.ingest = Ingest.objects.create(file_name='input_file', file_size=10, status='TRANSFERRING', 
+        ingest = Ingest.objects.create(file_name='input_file', file_size=10, status='TRANSFERRING', 
                                             bytes_transferred=10, transfer_started=now(), media_type='text/plain',
                                             ingest_started=now(), data_started=now(), 
-                                            workspace=self.workspace_1, new_workspace=self.workspace_2, 
-                                            data_type_tags=['type1'], source_file=self.source_file)
+                                            workspace=workspace_1, new_workspace=workspace_2, 
+                                            data_type_tags=['type1'], source_file=source_file)
 
         manifest = job_test_utils.create_seed_manifest(inputs_files=[{'name': 'INPUT_FILE', 'media_types': ['text/plain'], 'required': True, 'multiple': True}], inputs_json=[])
-        self.jt1 = job_test_utils.create_seed_job_type(manifest=manifest)
+        jt1 = job_test_utils.create_seed_job_type(manifest=manifest)
         recipe_type_def = {'version': '6',
                            'input': {'files': [{'name': 'INPUT_FILE',
                                                 'media_types': ['text/plain'],
@@ -54,58 +56,51 @@ class TestCreateIngest(TestCase):
                                     'json': []},
                            'nodes': {'node_a': {'dependencies': [],
                                                 'input': {'INPUT_FILE': {'type': 'recipe', 'input': 'INPUT_FILE'}},
-                                                'node_type': {'node_type': 'job', 'job_type_name': self.jt1.name,
-                                                              'job_type_version': self.jt1.version,
+                                                'node_type': {'node_type': 'job', 'job_type_name': jt1.name,
+                                                              'job_type_version': jt1.version,
                                                               'job_type_revision': 1}}}}
 
-        self.recipe_type = recipe_test_utils.create_recipe_type_v6(name='test-recipe', definition=recipe_type_def)
+        recipe_type = recipe_test_utils.create_recipe_type_v6(name='test-recipe', definition=recipe_type_def)
 
         strike_config = {
             'version': '6',
-            'workspace': self.workspace_1.name,
+            'workspace': workspace_1.name,
             'monitor': {'type': 'dir-watcher', 'transfer_suffix': '_tmp'},
             'files_to_ingest': [{
                 'filename_regex': 'input_file',
                 'data_types': ['image_type'],
-                'new_workspace': self.workspace_2.name,
+                'new_workspace': workspace_2.name,
                 'new_file_path': 'my/path'
             }],
             'recipe': {
-                'name': self.recipe_type.name
+                'name': recipe_type.name
             },
         }
         config = StrikeConfigurationV6(strike_config).get_configuration()
-        self.strike = Strike.objects.create_strike('my_name', 'my_title', 'my_description', config)
+        strike = Strike.objects.create_strike('my_name', 'my_title', 'my_description', config)
 
-        scan_config = {
-            'workspace': self.workspace_1.name,
-            'scanner': {
-                'type': 'dir'
-            },
-            'files_to_ingest': [{
-                'filename_regex': 'input_file',
-                'data_types': ['type1'],
-                'new_file_path': os.path.join('my', 'path'),
-                'new_workspace': self.workspace_2.name,
-            }],
-            'recipe': {
-                'name': self.recipe_type.name,
-            },
-        }
-        scan_configuration = ScanConfigurationV6(scan_config).get_configuration()
-        self.scan = Scan.objects.create_scan('my_name', 'my_title', 'my_description', scan_configuration)
-        
-    @patch('queue.models.create_process_job_input_messages')
-    @patch('queue.models.CommandMessageManager')
-    @patch('ingest.messages.create_ingest_jobs.create_strike_ingest_job_message')
-    def test_json_create(self, mock_create, mock_msg_mgr, mock_process):
-        """Tests converting a CreateIngest message to and from json
-        """
+        # scan_config = {
+        #     'workspace': workspace_1.name,
+        #     'scanner': {
+        #         'type': 'dir'
+        #     },
+        #     'files_to_ingest': [{
+        #         'filename_regex': 'input_file',
+        #         'data_types': ['type1'],
+        #         'new_file_path': os.path.join('my', 'path'),
+        #         'new_workspace': workspace_2.name,
+        #     }],
+        #     'recipe': {
+        #         'name': recipe_type.name,
+        #     },
+        # }
+        # scan_configuration = ScanConfigurationV6(scan_config).get_configuration()
+        # scan = Scan.objects.create_scan('my_name', 'my_title', 'my_description', scan_configuration)
 
         message = CreateIngest()
         message.create_ingest_type = STRIKE_JOB_TYPE
-        message.ingest_id = self.ingest.id
-        message.strike_id = self.strike.id
+        message.ingest_id = ingest.id
+        message.strike_id = strike.id
 
         # Convert message to JSON and back, and then execute
         message_dict = message.to_json()
@@ -119,11 +114,11 @@ class TestCreateIngest(TestCase):
         job_data = job.get_input_data()
         for value in job_data.values:
             if value == 'ingest_id':
-                self.assertEqual(job_data.values[value].value, self.ingest.id)
+                self.assertEqual(job_data.values[value].value, ingest.id)
             elif value == 'workspace':
-                self.assertEqual(job_data.values[value].value, self.workspace_1.name)
+                self.assertEqual(job_data.values[value].value, workspace_1.name)
             elif value == 'new_workspace':
-                self.assertEqual(job_data.values[value].value, self.workspace_2.name)
+                self.assertEqual(job_data.values[value].value, workspace_2.name)
             
         # Verify job has been queueud
         from queue.models import Queue
