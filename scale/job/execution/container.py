8e9369e99264527ae2fcf3621e23b0c1278310c4
Mike Holt <mike.n.holt@gmail.com>
2019-09-10 22:38:13 -0400

    Input metadata small fix (#1781)
    * added file creation logic
    
    * fixed test
    
    * removed tmp folder
    
    * removed mkdir statement

--- scale/job/execution/container.py
+++ scale/job/execution/container.py
@@ -8,7 +8,7 @@ from storage.container import SCALE_ROOT_PATH
 
 SCALE_JOB_EXE_INPUT_PATH = os.path.join(SCALE_ROOT_PATH, 'input_data')
 SCALE_JOB_EXE_OUTPUT_PATH = os.path.join(SCALE_ROOT_PATH, 'output_data')
-SCALE_INPUT_METADATA_PATH = os.path.join(SCALE_ROOT_PATH, 'input_data', 'tmp', 'scale-input-metadata.json')
+SCALE_INPUT_METADATA_PATH = os.path.join(SCALE_ROOT_PATH, 'input_data', 'scale-input-metadata.json')
 
 
 def get_job_exe_input_vol_name(job_exe):
