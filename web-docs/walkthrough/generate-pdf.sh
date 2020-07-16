8661ea8b3204241c403bf641dc96191a69cf7341
Matt Anderson <matt@mattbanderson.com>
2020-07-16 16:35:26 -0400

    Add table of contents to Scale walkthrough and styles to PDF output

--- web-docs/walkthrough/generate-pdf.sh
+++ web-docs/walkthrough/generate-pdf.sh
@@ -1,5 +1,5 @@
 echo Generating PDF...
-asciidoctor-pdf -D /documents/output index.adoc
+asciidoctor-pdf -a stylesdir=./styles -a stylesheet=walkthrough.css -D /documents/output index.adoc
 
 echo Rename output PDF...
 mv /documents/output/index.pdf /documents/output/scale-walkthrough.pdf
