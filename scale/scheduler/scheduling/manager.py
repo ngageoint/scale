5395e94ab435bad7a3ac7b2a7a433295b6c721ff
matttalda <67440050+matttalda@users.noreply.github.com>
2020-08-18 08:06:36 -0400

    Sharedmem skip (#1907)
    * sharedmem added to invalid_resources check
    
    * scheduler properly stops jobs with sharedmem > 0
    
    * removed sharedmem from MIN_RESOURCE
    
    * reverted last commit and made a check for zero value sharemem resources
    
    * reverted model to always strip sharemem resource
    
    * reverted comment

--- scale/scheduler/scheduling/manager.py
+++ scale/scheduler/scheduling/manager.py
@@ -368,10 +368,11 @@ class SchedulingManager(object):
             insufficient_resources = []
             # get resource names offered and compare to job type resources
             for resource in job_exe.required_resources.resources:
-                # skip sharedmem
-                if resource.name.lower() == 'sharedmem':
-                    continue
-                if resource.name not in max_cluster_resources._resources:
+                # Check for invalid resource or sharedmem
+                if (resource.name not in max_cluster_resources._resources) or (resource.name.lower() == 'sharedmem'):
+                    # Skip sharedmem if its 0
+                    if (resource.name.lower() == 'sharedmem') and (resource.value <= 0):
+                        continue
                     if jt.name in type_warnings:
                         type_warnings[jt.name]['count'] += 1
                         if resource.name not in type_warnings[jt.name]['warning']:
